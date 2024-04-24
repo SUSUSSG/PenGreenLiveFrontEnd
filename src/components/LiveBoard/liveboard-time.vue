5d37eb21793a2266be5b1737e9b9cbf608022074
JiniiW <ksjine2@gmail.com>
2024-04-24 20:36:36 +0900

    feat : 아이콘 생성

--- src/components/LiveBoard/liveboard-time.vue
+++ src/components/LiveBoard/liveboard-time.vue
@@ -1,30 +1,34 @@
 <template>
-  <div v-if="isBraodcasting">
-    <Button v-on:click="toggleBroadcase()">방송 종료</Button>
-    <Button text="danger" btnClass="btn-outline-danger rounded-[999px]">방송 중</Button>
+  <div v-if="isBroadcasting">
+    <Button v-on:click="toggleBroadcast()">라이브 종료</Button>
+    <Button text="danger" btnClass="btn-outline-danger rounded-[999px]">라이브</Button>
   </div>
   <div v-else>
-    <Button v-on:click="toggleBroadcase()" text="Dark" btnClass="btn-dark">방송 시작</Button>
-    <Button text="dark" btnClass="btn-outline-dark rounded-[999px]">방송 대기</Button>
+    <Button v-on:click="toggleBroadcast()" text="Dark" btnClass="btn-dark">라이브 시작</Button>
+    <Button text="dark" btnClass="btn-outline-dark rounded-[999px]">대기</Button>
   </div>
   <div class="row">
-    <div v-for="(item, i) in statistics" v-on:key="i">
-      <Card>
-        <div>
-          <div></div>
-          <div>
-            <div class="">
+    <div v-for="(item, i) in statistics" :key="i">
+      <div class="flex space-x-3 rtl:space-x-reverse">
+        <div class="flex-none">
+          <div class="bg-white rounded pt-3 px-4">
+            <div class="h-12 w-12 rounded-full flex flex-col items-center justify-center text-2xl"
+              :class="`${item.bg} ${item.text}`">
+              <Icon :icon="`heroicons:clock`"></Icon>
+            </div>
+            <div class="text-sm text-slate-600 dark:text-slate-300 mb-[6px]">
               {{ item.title }}
             </div>
-            <div class="">
+            <div class="text-lg text-slate-900 dark:text-white font-medium mb-[6px]">
               {{ item.time }}
             </div>
           </div>
         </div>
-      </Card>
+      </div>
     </div>
   </div>
 </template>
+
 <script>
 import Card from "@/components/Card";
 import Icon from "@/components/Icon";
@@ -43,39 +47,24 @@ export default {
           name: "time1",
           title: "현재시간",
           time: "00:00:00",
-          text: "text-white",
-          icon: "clock",
         },
         {
           name: "time2",
           title: "경과시간",
           time: "00:00:00",
-          text: "text-white",
-          icon: "clock",
         },
         {
           name: "time3",
           title: "남은시간",
           time: "00:00:00",
-          text: "text-white",
-          icon: "clock",
         }
       ],
-      isBraodcasting: false,
-    }
-  },
-  computed: {
-    broadcastAction() {
-      return this.isBraodcasting ? "방송종료" : "방송시작";
-    },
-    broadcastStatus() {
-      return this.isBraodcasting ? "방송중" : "방송대기";
+      isBroadcasting: false,
     }
   },
   methods: {
-    toggleBroadcase() {
-      this.isBraodcasting = !this.isBraodcasting;
-      //console.log("isBraodcasting 상태:", this.isBraodcasting);
+    toggleBroadcast() {
+      this.isBroadcasting = !this.isBroadcasting;
     }
   }
 };
