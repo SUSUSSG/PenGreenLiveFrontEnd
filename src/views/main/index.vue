378e4cf85c66e910168a59521d991ab8523ab3ec
zzzjinwook <zzzjinwook@naver.com>
2024-04-24 18:55:03 +0900

    feat : 슬라이드 종류 변경

--- src/views/main/index.vue
+++ src/views/main/index.vue
@@ -3,21 +3,28 @@
     <div>메인페이지입니다.</div>
     <swiper
       :modules="modules"
-      :slides-per-view="sliderPreview"
-      :space-between="space"
-      navigation
+      :slides-per-view="3"
+      :space-between="10"
+      :effect="'coverflow'"
+      :coverflowEffect="{
+        rotate: 50,
+        stretch: 0,
+        depth: 100,
+        modifier: 1,
+        slideShadows: true,
+      }"
       :pagination="{ clickable: true }"
       class="main-caro"
     >
       <swiper-slide v-for="(item, i) in carousels" :key="i">
         <div
-          class="single-slide bg-no-repeat bg-cover bg-center rounded-md min-h-[300px]"
+          class="single-slide bg-no-repeat bg-cover bg-center rounded-md min-h-[400px]"
           :style="{
             backgroundImage: 'url(' + item.img + ')',
           }"
         >
           <div
-            class="pt-20 container text-center px-4 slider-content h-full w-full min-h-[300px] rounded-md flex flex-col items-center justify-center text-white"
+            class="pt-20 container text-center px-4 slider-content h-full w-full min-h-[400px] rounded-md flex flex-col items-center justify-center text-white"
           >
             <div class="max-w-sm">
               <h2 v-if="item.title" class="text-xl font-medium text-white">
@@ -42,7 +49,7 @@ import {
   Scrollbar,
   A11y,
   Autoplay,
-  EffectFade,
+  EffectCoverflow,
 } from "swiper";
 
 // Import Swiper Vue.js components
@@ -53,7 +60,7 @@ import "swiper/css";
 import "swiper/css/navigation";
 import "swiper/css/pagination";
 import "swiper/css/scrollbar";
-import "swiper/css/effect-fade";
+import "swiper/css/effect-coverflow";
 
 export default {
   components: {
@@ -78,31 +85,31 @@ export default {
     return {
       carousels: [
         {
-          img: 'https://via.placeholder.com/1920x1080?text=Slide+1',
-          title: '첫 번째 슬라이드',
-          description: '이것은 첫 번째 슬라이드에 대한 설명입니다.',
+          img: "https://i.namu.wiki/i/aC6h0MKu3PAAegyUIPps5zOdQ1efqgNSTHKDzj1BL-bB0n8IZQALvtXp73MNCasoMpYCGKIQ4C8WWi3_E7-kwg.gif",
+          title: "첫 번째 슬라이드",
+          description: "이것은 첫 번째 슬라이드에 대한 설명입니다.",
         },
         {
-          img: 'https://via.placeholder.com/1920x1080?text=Slide+2',
-          title: '두 번째 슬라이드',
-          description: '두 번째 슬라이드에 대한 설명입니다.',
+          img: "https://i.ytimg.com/vi/7DSJl9e-UUE/maxresdefault.jpg",
+          title: "두 번째 슬라이드",
+          description: "두 번째 슬라이드에 대한 설명입니다.",
         },
         {
-          img: 'https://via.placeholder.com/1920x1080?text=Slide+3',
-          title: '세 번째 슬라이드',
-          description: '세 번째 슬라이드의 설명입니다.',
+          img: "http://via.placeholder.com/800x500",
+          title: "세 번째 슬라이드",
+          description: "세 번째 슬라이드의 설명입니다.",
         },
         {
-          img: 'https://via.placeholder.com/1920x1080?text=Slide+4',
-          title: '네 번째 슬라이드',
-          description: '마지막 슬라이드에 대한 설명입니다.',
+          img: "https://i.ytimg.com/vi/h8eQxRIrK_s/maxres2.jpg?sqp=-oaymwEoCIAKENAF8quKqQMcGADwAQH4AbYIgAK4CIoCDAgAEAEYSiBYKGUwDw==&rs=AOn4CLClrIaPLDGOqflkWdL_AzMByrUgDw",
+          title: "네 번째 슬라이드",
+          description: "마지막 슬라이드에 대한 설명입니다.",
         },
       ],
     };
   },
   setup() {
     return {
-      modules: [Navigation, Pagination, Scrollbar, A11y, Autoplay, EffectFade],
+      modules: [Navigation, Pagination, Scrollbar, A11y, Autoplay, EffectCoverflow],
     };
   },
 };
@@ -128,6 +135,7 @@ export default {
     height: 2px;
     width: 24px;
     @apply rounded-[1px] bg-white bg-opacity-70;
+
     &.swiper-pagination-bullet-active {
       @apply bg-opacity-100;
     }
