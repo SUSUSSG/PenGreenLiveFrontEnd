4ad3320b2c12c6879d0e0ee126e86996a035e287
JiniiW <ksjine2@gmail.com>
2024-04-25 21:32:44 +0900

    feat : 프롬프터 구조 및 영역 구현

---
+++ src/components/LiveBoard/liveboard-prompt.vue
@@ -0,0 +1,29 @@
+<template>
+    <div class="bg-white rounded pt-3 px-4 mt-4 ml-5" id="promptCard">
+        <div class="text-base mb-3">
+            프롬프터
+        </div>
+        <Textarea name="largesize" placeholder="여기에 작성해주세요" rows="8"></Textarea>
+    </div>
+</template>
+  
+<script>
+import Textarea from "@/components/Textarea";
+export default {
+    components: {
+        Textarea
+    },
+    data() {
+        return {
+
+        }
+    }
+}
+</script>
+  
+<style>
+#promptCard {
+    width: 800px;
+    height: 250px;
+}
+</style>
--- src/views/broadcast/liveboard.vue
+++ src/views/broadcast/liveboard.vue
@@ -1,4 +1,5 @@
 <template>
+    <LiveboardPrompt></LiveboardPrompt>
     <LiveBoardStatistics></LiveBoardStatistics>
     <LiveBoardTime></LiveBoardTime>
     <LiveBoardChat></LiveBoardChat>
@@ -9,13 +10,15 @@ import LiveBoardTime from '@/components/LiveBoard/liveboard-time.vue';
 import LiveBoardChat from '@/components/LiveBoard/liveboard-chat.vue';
 import LiveboardBroad from '@/components/LiveBoard/liveboard-broad.vue';
 import LiveBoardStatistics from '@/components/LiveBoard/liveboard-statistics.vue';
+import LiveboardPrompt from '@/components/LiveBoard/liveboard-prompt.vue';
 
 export default {
     components: {
         LiveBoardTime,
         LiveBoardChat,
         LiveboardBroad,
-        LiveBoardStatistics
+        LiveBoardStatistics,
+        LiveboardPrompt
     }
 };
 </script>
