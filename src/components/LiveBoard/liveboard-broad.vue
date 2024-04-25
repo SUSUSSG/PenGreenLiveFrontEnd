c42d999922a0a19eb3c1d0ed489d8e2867283f87
JiniiW <ksjine2@gmail.com>
2024-04-25 16:59:45 +0900

    feat : 송출화면 구조 구현

---
+++ src/components/LiveBoard/liveboard-broad.vue
@@ -0,0 +1,23 @@
+<template>
+    <div class="bg-white rounded pt-3 px-4 mt-4 ml-5" id="broadCard">
+        <div class="text-base">
+                송출 화면
+        </div>
+    </div>
+</template>
+<script>
+export default {
+    components: {
+
+    },
+    data() {
+
+    }
+}
+</script>
+<style>
+#broadCard {
+    width: 400px;
+    height: 500px;
+}
+</style>
--- src/views/broadcast/liveboard.vue
+++ src/views/broadcast/liveboard.vue
@@ -1,15 +1,18 @@
 <template>
     <LiveBoardTime></LiveBoardTime>
     <LiveBoardChat></LiveBoardChat>
+    <LiveboardBroad></LiveboardBroad>
 </template>
 <script>
 import LiveBoardTime from '@/components/LiveBoard/liveboard-time.vue';
 import LiveBoardChat from '@/components/LiveBoard/liveboard-chat.vue';
+import LiveboardBroad from '@/components/LiveBoard/liveboard-broad.vue';
 
 export default {
     components: {
         LiveBoardTime,
-        LiveBoardChat
+        LiveBoardChat,
+        LiveboardBroad
     }
 };
 </script>
