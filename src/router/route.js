f2463f312bd250feda4ed6476ec0a68cd043db6f
SonHyeJi <kuy06046@naver.com>
2024-04-26 16:26:12 +0900

    style : 불필요 주석 제거

--- src/router/route.js
+++ src/router/route.js
@@ -2,7 +2,6 @@ const routes = [
   {
     path: "/",
     name: "Layout",
-    // redirect: "/home",
     component: () => import("@/Layout/main-index.vue"),
     children: [
       {
@@ -10,51 +9,21 @@ const routes = [
         name: "home",
         component: () => import("@/views/main/index.vue"),
       },
-      // {
-      //   path: "/broadcast/live-register",
-      //   name: "register-live",
-      //   component: () => import("@/views/broadcast/register-live.vue")
-      // }
     ],
   },
-  // {
-  //   path: "/main",
-  //   components: {
-  //     default: () => import("@/Layout/main-index.vue"),
-  //   },
-  //   children: [
-  //     {
-  //       path: "",
-  //       name: "main-home",
-  //       component: () => import("@/views/main/index.vue"),
-  //     },
-  //   ],
-  // },
-  // {
-  //   path: "/",
-  //   name: "main",
-  //   component: () => import("@/Layout/main-index.vue"),
-  //   children: [
-  //     {
-  //       path: "",
-  //       name: "main",
-  //       component: () => import("@/views/main/index.vue"),
-  //     },
-  //   ],
-  // },
   {
-    path:"/", 
-    component: () => import("@/Layout/index.vue"), 
+    path: "/",
+    component: () => import("@/Layout/index.vue"),
     children: [
       {
         path: "product-list",
         name: "product-list",
-        component: () => import("@/views/dashboard/dashboard-index.vue") 
+        component: () => import("@/views/dashboard/dashboard-index.vue")
       },
       {
-        path: "live-register", 
+        path: "live-register",
         name: "live-register",
-        component: () => import("@/views/broadcast/live-register.vue") 
+        component: () => import("@/views/broadcast/live-register.vue")
       }
     ]
   },
