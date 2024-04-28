7c9647bf3add8d6d3a37b1362d1a26d9df7a80de
SonHyeJi <kuy06046@naver.com>
2024-04-28 18:54:03 +0900

    css : 상품 카테고리 대분류 클릭 이벤트 설정

--- src/components/Product/product-category.vue
+++ src/components/Product/product-category.vue
@@ -5,7 +5,6 @@
       <div class="text-base text-slate-600 dark:text-slate-300">
         <Textinput label="카테고리 " type="text" name="productcategory" v-model="productcategory"
           placeholder="추가 원하는 카테고리명을 입력하세요" class="mb-2" />
-
       </div>
       <template v-slot:footer>
         <Button text="등록" btnClass="btn-dark btn-sm" @click="$refs.modal1.closeModal(); resetModalData()" />
@@ -21,18 +20,17 @@
       <header class="px-4 pt-4 pb-3 mb-3">
         <h5 class="card-title mb-0">대분류 카테고리</h5>
       </header>
-      <vue-good-table :columns="columns" :rows="firstTableData" styleClass="vgt-table"
-        :sort-options="{ enabled: false }" :row-style-class="rowStyleClass" id="categorytable">
-        <template v-slot:table-row="props">
-          <div>
-            <span v-if="props.column.field == 'firstcategory'"
-              :class="{ 'selected-category': selectedCategory === props.row.firstcategory }"
-              @click="selectCategory(props.row.firstcategory)">
-              {{ props.row.firstcategory }}
-            </span>
-          </div>
-        </template>
-      </vue-good-table>
+      <vue-good-table :columns="columns" :rows="firstTableData" styleClass="vgt-table2"
+    :sort-options="{ enabled: false }" :row-style-class="rowStyleClass" id="categorytable">
+    <template v-slot:table-row="props">
+      <div :class="{ 'selected-row': selectedCategory === props.row.firstcategory }" style="padding : 1rem;">
+        <span v-if="props.column.field == 'firstcategory'"
+          @click="selectCategory(props.row.firstcategory)">
+          {{ props.row.firstcategory }}
+        </span>
+      </div>
+    </template>
+  </vue-good-table>
     </Card>
 
     <Card bodyClass="p-0">
@@ -73,6 +71,7 @@ export default {
   data() {
     return {
       selectedCategory: null,
+      active_row: null,
       firstTableData,
       columns: [
         {
@@ -90,10 +89,10 @@ export default {
       ]
     };
   },
+
   methods: {
     selectCategory(category) {
       this.selectedCategory = category;
-      this.$forceUpdate();
     }
   },
   computed: {
@@ -104,18 +103,42 @@ export default {
       return this.secondTableData.filter(item => item.id < 8);
     },
   },
-  rowStyleClass(row) { if (this.selectedCategory === row.firstcategory) { return 'selected-category'; } return ''; }
+  rowStyleClass(row) {
+    if (this.active_row === row.firstcategory) {
+      return 'selected-row';
+    }
+    return '';
+  }
 };
 </script>
+
 <style lang="scss">
 .vgt-inner-wrap {
   @apply shadow-none;
 }
 
-.selected-category td {
-  background-color: #b97f7f; // 원하는 색상으로 변경하세요
+.vgt-table2 {
+  --tw-border-opacity: 1;
+    border-color: rgb(241 245 249 / var(--tw-border-opacity));
+   padding: 0 !important;
+    font-size: 0.875rem;
+    line-height: 1.25rem;
+    font-weight: 400;
+    text-transform: capitalize;
+    --tw-text-opacity: 1;
+    color: rgb(71 85 105 / var(--tw-text-opacity));
+  width: 100%;
+  
+
+}
+.selected-category {
+  background-color: #f0f0f0;
+  font-weight: bold;
 }
 
+.selected-row {
+  background-color: #f0f0f0; /* 선택된 행의 배경색 변경 */
+}
 #categorytable thead{
   display : none;
 }
