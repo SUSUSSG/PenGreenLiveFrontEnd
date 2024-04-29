ca091463866da3183e1ea302591218342d30aea6
JiniiW <ksjine2@gmail.com>
2024-04-29 11:50:53 +0900

    feat : 네비게이션바 기본 모달 띄우기

--- src/components/LiveBoard/liveboard-sidebar.vue
+++ src/components/LiveBoard/liveboard-sidebar.vue
@@ -1,20 +1,58 @@
 <template>
     <div class="bg-white rounded pt-3 px-4 mt-4 ml-5" id="sidebar">
         <div class="grid gap-8 mt-5" style="grid-template-rows: repeat(6, 1fr);">
-            <div v-for="(item, i) in statistics" :key="i"
-                class="mx-auto h-10 w-10 flex flex-col items-center justify-center rounded-full bg-white text-2xl mb-4 black"
-                v-on:click="toggleIcon(item)">
-                <Icon :icon="item.isActive ? item.activeIcon : item.icon"></Icon>
+            <div class="grid gap-8 mt-5" style="grid-template-rows: repeat(6, 1fr);">
+
+                <div class="mx-auto h-10 w-10 flex flex-col items-center justify-center rounded-full bg-white text-2xl mb-4 black cursor-pointer"
+                    @click="toggleIcon(statistics[0])">
+                    <Icon :icon="statistics[0].isActive ? statistics[0].activeIcon : statistics[0].icon"></Icon>
+                </div>
+
+                <div class="mx-auto h-10 w-10 flex flex-col items-center justify-center rounded-full bg-white text-2xl mb-4 black cursor-pointer"
+                    @click="toggleIcon(statistics[1])">
+                    <Icon :icon="statistics[1].isActive ? statistics[1].activeIcon : statistics[1].icon"></Icon>
+                </div>
+
+                <div class="mx-auto h-10 w-10 flex flex-col items-center justify-center rounded-full bg-white text-2xl mb-4 black cursor-pointer"
+                     @click="toggleIcon(statistics[2]); broadcastDeviceControl()">
+                    <Icon :icon="statistics[2].icon"></Icon>
+                    <Modal title="방송 기기 설정" ref="broadcastDeviceControl" :showButtons="false">
+                    </Modal>
+                </div>
+
+                <div class="mx-auto h-10 w-10 flex flex-col items-center justify-center rounded-full bg-white text-2xl mb-4 black cursor-pointer"
+                    @click="toggleIcon(statistics[3]); editBroadcastInfo()">
+                    <Icon :icon="statistics[3].icon"></Icon>
+                    <Modal title="방송 정보 편집" ref="editBroadcastInfo" :showButtons="false">
+                    </Modal>
+                </div>
+
+                <div class="mx-auto h-10 w-10 flex flex-col items-center justify-center rounded-full bg-white text-2xl mb-4 black cursor-pointer"
+                    @click="toggleIcon(statistics[4]); addNotice()">
+                    <Icon :icon="statistics[4].icon"></Icon>
+                    <Modal title="공지사항 등록" ref="addNotice" :showButtons="false">
+                    </Modal>
+                </div>
+
+                <div class="mx-auto h-10 w-10 flex flex-col items-center justify-center rounded-full bg-white text-2xl mb-4 black cursor-pointer"
+                    @click="toggleIcon(statistics[5]); addFaQ()">
+                    <Icon :icon="statistics[5].icon"></Icon>
+                    <Modal title="자주 묻는 질문 등록" ref="addFaQ" :showButtons="false">
+                    </Modal>
+                </div>
             </div>
         </div>
     </div>
 </template>
 
 <script>
-import Icon from "@/components/Icon"
+import Icon from "@/components/Icon";
+import Modal from "../Modal/Modal.vue";
+
 export default {
     components: {
-        Icon
+        Icon,
+        Modal
     },
     data() {
         return {
@@ -31,23 +69,23 @@ export default {
                 },
                 {
                     icon: "material-symbols:settings-video-camera-sharp",
-                    activeIcon: "material-symbols:settings-video-camera-sharp",
-                    isActive: false
+                    // activeIcon: "material-symbols:settings-video-camera-sharp",
+                    // isActive: false
                 },
                 {
                     icon: "material-symbols:app-registration",
-                    activeIcon: "material-symbols:app-registration",
-                    isActive: false
+                    // activeIcon: "material-symbols:app-registration",
+                    // isActive: false
                 },
                 {
                     icon: "material-symbols:campaign-rounded",
-                    activeIcon: "material-symbols:campaign-rounded",
-                    isActive: false
+                    // activeIcon: "material-symbols:campaign-rounded",
+                    // isActive: false
                 },
                 {
                     icon: "material-symbols:maps-ugc-rounded",
-                    activeIcon: "material-symbols:maps-ugc-rounded",
-                    isActive: false
+                    // activeIcon: "material-symbols:maps-ugc-rounded",
+                    // isActive: false
                 },
             ]
         }
@@ -55,6 +93,22 @@ export default {
     methods: {
         toggleIcon(item) {
             item.isActive = !item.isActive;
+        },
+        broadcastDeviceControl() {
+            this.isOpen = true;
+            this.$refs.broadcastDeviceControl.openModal();
+        },
+        editBroadcastInfo() {
+            this.isOpen = true;
+            this.$refs.editBroadcastInfo.openModal();
+        },
+        addNotice() {
+            this.isOpen = true;
+            this.$refs.addNotice.openModal();
+        },
+        addFaQ() {
+            this.isOpen = true;
+            this.$refs.addFaQ.openModal();
         }
     }
 }
