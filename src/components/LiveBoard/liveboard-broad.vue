f16406b3eebaa5fd50f4f97e43a0e212ac4dd114
JiniiW <ksjine2@gmail.com>
2024-04-29 19:06:35 +0900

    feat : 송출화면 컴포넌트 연결

--- src/components/LiveBoard/liveboard-broad.vue
+++ src/components/LiveBoard/liveboard-broad.vue
@@ -3,15 +3,22 @@
         <div class="text-base">
                 송출 화면
         </div>
+        <videoPlayer :videoSource="videoSourceSrc"></videoPlayer>
     </div>
 </template>
 <script>
+import videoPlayer from "@/components/Video/videoplayer.vue";
+import videoSource from "@/assets/video/test-video.mp4";
+
 export default {
     components: {
-
+        videoPlayer
     },
     data() {
-
+        return {
+            videoSourceSrc : videoSource
+        }
+        
     }
 }
 </script>
@@ -25,5 +32,9 @@ export default {
   border-radius: 0.5rem;
   background: white;
   box-shadow: 0 0.25rem 0.5rem rgba(0, 0, 0, 0.1);
+  padding-bottom: 10px;
+}
+.text-base {
+    margin-bottom: 5px;
 }
 </style>
