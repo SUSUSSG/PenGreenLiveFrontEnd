c17d9024510b408028f50264805901552b6ccbee
zzzjinwook <zzzjinwook@naver.com>
2024-04-29 15:13:16 +0900

    feat : 카테고리 아이콘 변경

--- src/components/Category/CategoryItem.vue
+++ src/components/Category/CategoryItem.vue
@@ -1,6 +1,27 @@
 <template>
-  <div style="display:flex; flex-direction:column; justify-content: center; align-items: center;" @click="handleClick">
-    <img :src="imgSrc" :class="{'rounded-img': isSelected}" alt="Description" style="width: 64px; height: 64px; border-radius:50%;" />
+  <div
+    style="
+      display: flex;
+      flex-direction: column;
+      justify-content: center;
+      align-items: center;
+    "
+    @click="handleClick"
+  >
+    <img
+      :src="imgSrc"
+      :class="{ 'rounded-img': isSelected }"
+      alt="Description"
+      style="
+        width: 80px;
+        height: 80px;
+        border-radius: 50%;
+        padding: 0.5rem;
+        background-color: #f0f0f0;
+      "
+      @mouseover="scaleUp"
+      @mouseout="scaleDown"
+    />
     <span>{{ name }}</span>
   </div>
 </template>
@@ -10,18 +31,36 @@ export default {
   props: {
     imgSrc: String,
     name: String,
-    isSelected: Boolean
+    isSelected: Boolean,
   },
   methods: {
     handleClick() {
-      this.$emit('select', this.name);
-    }
-  }
-}
+      this.$emit("select", this.name);
+    },
+    scaleUp(event) {
+      event.target.style.transform = "scale(1.1)";
+    },
+    scaleDown(event) {
+      event.target.style.transform = "scale(1)";
+    },
+  },
+};
 </script>
 
-<style>
+<style scoped>
 .rounded-img {
-  border: 3px solid greenyellow;
+  border: 2px solid #1c6d16;
+  transition: transform 0.2s ease;
+}
+
+img {
+  transition: transform 0.2s ease;
+  border: 2px solid #f0f0f0;
+}
+
+img:hover {
+  transition: transform 0.2s ease;
+  transform: scale(1.1);
+  border: 2px solid lightgray;
 }
 </style>
