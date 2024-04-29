075f1fe7e89400ba65fc8cac376f23d491a1ada7
zzzjinwook <zzzjinwook@naver.com>
2024-04-29 11:24:36 +0900

    feat : 일정표 페이지 컴포넌트 호출

--- src/components/Card/schedule-card.vue
+++ src/components/Card/schedule-card.vue
@@ -1,43 +1,76 @@
 <template>
   <div class="group" style="display: flex !important">
     <div class="flex-row-wrapper">
-      <h6 class="live-time">09:10</h6>
-      <img
-        class="live-thumbnail-image"
-        src="https://g-selected.pstatic.net/MjAyNDA0MjJfNjMg/MDAxNzEzNzQyMDA2NjEx.UWvsFEHFzOoSUWwKGXJIl3T7tJrNRte4Bc39WLbc0gEg.5EipWVepAxebwdX8HRURM4kiI0phN1juuk3IkQVvyycg.JPEG/image.jpg?type=f320_480_q90"
-        alt="Image"
-      />
+      <h6 class="live-time">{{ liveTime }}</h6>
+      <img class="live-thumbnail-image" :src="thumbnailUrl" alt="Image" />
       <div class="content-wrapper">
-        <div class="live-title">live title test</div>
-        <div class="live-benefit-title">라이브 한정 추가 쿠폰 제공</div>
+        <div class="live-title">{{ liveTitle }}</div>
+        <div class="live-benefit-title">{{ liveBenefitTitle }}</div>
         <hr class="mt-2 mb-2" />
         <div class="product-wrapper">
-          <img
-            class="live-product-image"
-            src="https://g-selected.pstatic.net/MjAyNDA0MjJfNjMg/MDAxNzEzNzQyMDA2NjEx.UWvsFEHFzOoSUWwKGXJIl3T7tJrNRte4Bc39WLbc0gEg.5EipWVepAxebwdX8HRURM4kiI0phN1juuk3IkQVvyycg.JPEG/image.jpg?type=f320_480_q90"
-            alt="Image"
-          />
+          <img class="live-product-image" :src="productImgUrl" alt="Image" />
           <div class="product-content-wrapper">
             <div class="product-title">
-              상품 이름 - 상품 설명이 위치하는 영역입니다.
+              {{ productName }} - {{ productDescription }}
             </div>
-            <div class="product-price">60% 할인 123,450원</div>
+            <div class="product-price">60% 할인 {{ productPrice }}</div>
           </div>
         </div>
       </div>
     </div>
-
     <div class="image-wrapper">
-      <img
-        class="live-thumbnail-image"
-        src="https://phinf.pstatic.net/dthumb/?src=%22http%3A%2F%2Fshop1.phinf.naver.net%2F20220412_182%2F1649725677134MWG9L_PNG%2F50861519847511263_1050462988.png%22&service=selective&type=f180_180_q90"
-        alt="Image"
-      />
-      <div class="list-one-line">testshop</div>
+      <img class="live-thumbnail-image" :src="shopThumbnailUrl" alt="Image" />
+      <div class="list-one-line">{{ shopName }}</div>
     </div>
   </div>
 </template>
 
+<script>
+export default {
+  props: {
+    liveTime: {
+      type: String,
+      required: true,
+    },
+    thumbnailUrl: {
+      type: String,
+      required: true,
+    },
+    productImgUrl: {
+      type: String,
+      required: true,
+    },
+    liveTitle: {
+      type: String,
+      required: true,
+    },
+    liveBenefitTitle: {
+      type: String,
+      required: true,
+    },
+    productName: {
+      type: String,
+      required: true,
+    },
+    productDescription: {
+      type: String,
+      required: true,
+    },
+    productPrice: {
+      type: String,
+      required: true,
+    },
+    shopName: {
+      type: String,
+      required: true,
+    },
+    shopThumbnailUrl: {
+      type: String,
+      required: true,
+    },
+  },
+};
+</script>
 <style scoped>
 .content-wrapper {
   width: 100%;
@@ -52,6 +85,7 @@
 .image-wrapper img {
   width: 60px;
   height: 60px;
+  object-fit: cover;
 }
 .flex-row-wrapper {
   width: 100%;
@@ -68,10 +102,11 @@
   align-items: center;
   height: 100%;
   gap: 1rem;
-  padding-right: 2rem;
+  padding-right: 5rem;
 }
 .live-thumbnail-image {
   max-height: 240px;
+  border-radius: 12px;
 }
 .live-title {
   font-size: 20px;
--- src/views/main/schedule.vue
+++ src/views/main/schedule.vue
@@ -1,9 +1,24 @@
 <template>
   <div class="content-wrapper">
-    <CardSchedule />
     <Categories />
     <hr class="mt-6" />
 
+    <CardSchedule
+      v-for="(item, index) in liveData"
+      :key="index"
+      :liveTime="item.liveTime"
+      :thumbnailUrl="item.thumbnailUrl"
+      :liveTitle="item.liveTitle"
+      :liveBenefitTitle="item.liveBenefitTitle"
+      :productName="item.productName"
+      :productImgUrl="item.productImgUrl"
+      :productDescription="item.productDescription"
+      :productPrice="item.productPrice"
+      :shopName="item.shopName"
+      :shopThumbnailUrl="item.shopThumbnailUrl"
+      class="mt-8"
+    />
+    <hr class="mt-6" />
     <hr />
   </div>
 </template>
@@ -18,19 +33,83 @@ export default {
     CardSchedule,
   },
   data() {
-    return {};
+    return {
+      liveData: [
+        {
+          liveTime: "09:10",
+          thumbnailUrl: "https://g-selected.pstatic.net/MjAyNDA0MjJfNjMg/MDAxNzEzNzQyMDA2NjEx.UWvsFEHFzOoSUWwKGXJIl3T7tJrNRte4Bc39WLbc0gEg.5EipWVepAxebwdX8HRURM4kiI0phN1juuk3IkQVvyycg.JPEG/image.jpg?type=f320_480_q90",
+          liveTitle: "live title test",
+          liveBenefitTitle: "라이브 한정 추가 쿠폰 제공",
+          productName: "상품 이름",
+          productImgUrl: "https://g-selected.pstatic.net/MjAyNDA0MjJfNjMg/MDAxNzEzNzQyMDA2NjEx.UWvsFEHFzOoSUWwKGXJIl3T7tJrNRte4Bc39WLbc0gEg.5EipWVepAxebwdX8HRURM4kiI0phN1juuk3IkQVvyycg.JPEG/image.jpg?type=f320_480_q90",
+          productDescription: "상품 설명이 위치하는 영역입니다.",
+          productPrice: "60% 할인 123,450원",
+          shopName: "testshop",
+          shopThumbnailUrl: "https://phinf.pstatic.net/dthumb/?src=%22http%3A%2F%2Fshop1.phinf.naver.net%2F20220412_182%2F1649725677134MWG9L_PNG%2F50861519847511263_1050462988.png%22&service=selective&type=f180_180_q90",
+        },
+        {
+          liveTime: "09:10",
+          thumbnailUrl: "https://g-selected.pstatic.net/MjAyNDA0MjJfNjMg/MDAxNzEzNzQyMDA2NjEx.UWvsFEHFzOoSUWwKGXJIl3T7tJrNRte4Bc39WLbc0gEg.5EipWVepAxebwdX8HRURM4kiI0phN1juuk3IkQVvyycg.JPEG/image.jpg?type=f320_480_q90",
+          liveTitle: "live title test",
+          liveBenefitTitle: "라이브 한정 추가 쿠폰 제공",
+          productName: "상품 이름",
+          productImgUrl: "https://g-selected.pstatic.net/MjAyNDA0MjJfNjMg/MDAxNzEzNzQyMDA2NjEx.UWvsFEHFzOoSUWwKGXJIl3T7tJrNRte4Bc39WLbc0gEg.5EipWVepAxebwdX8HRURM4kiI0phN1juuk3IkQVvyycg.JPEG/image.jpg?type=f320_480_q90",
+          productDescription: "상품 설명이 위치하는 영역입니다.",
+          productPrice: "60% 할인 123,450원",
+          shopName: "testshop",
+          shopThumbnailUrl: "https://phinf.pstatic.net/dthumb/?src=%22http%3A%2F%2Fshop1.phinf.naver.net%2F20220412_182%2F1649725677134MWG9L_PNG%2F50861519847511263_1050462988.png%22&service=selective&type=f180_180_q90",
+        },
+        {
+          liveTime: "09:10",
+          thumbnailUrl: "https://g-selected.pstatic.net/MjAyNDA0MjJfNjMg/MDAxNzEzNzQyMDA2NjEx.UWvsFEHFzOoSUWwKGXJIl3T7tJrNRte4Bc39WLbc0gEg.5EipWVepAxebwdX8HRURM4kiI0phN1juuk3IkQVvyycg.JPEG/image.jpg?type=f320_480_q90",
+          liveTitle: "live title test",
+          liveBenefitTitle: "라이브 한정 추가 쿠폰 제공",
+          productName: "상품 이름",
+          productImgUrl: "https://g-selected.pstatic.net/MjAyNDA0MjJfNjMg/MDAxNzEzNzQyMDA2NjEx.UWvsFEHFzOoSUWwKGXJIl3T7tJrNRte4Bc39WLbc0gEg.5EipWVepAxebwdX8HRURM4kiI0phN1juuk3IkQVvyycg.JPEG/image.jpg?type=f320_480_q90",
+          productDescription: "상품 설명이 위치하는 영역입니다.",
+          productPrice: "60% 할인 123,450원",
+          shopName: "testshop",
+          shopThumbnailUrl: "https://phinf.pstatic.net/dthumb/?src=%22http%3A%2F%2Fshop1.phinf.naver.net%2F20220412_182%2F1649725677134MWG9L_PNG%2F50861519847511263_1050462988.png%22&service=selective&type=f180_180_q90",
+        },
+        {
+          liveTime: "09:10",
+          thumbnailUrl: "https://g-selected.pstatic.net/MjAyNDA0MjJfNjMg/MDAxNzEzNzQyMDA2NjEx.UWvsFEHFzOoSUWwKGXJIl3T7tJrNRte4Bc39WLbc0gEg.5EipWVepAxebwdX8HRURM4kiI0phN1juuk3IkQVvyycg.JPEG/image.jpg?type=f320_480_q90",
+          liveTitle: "live title test",
+          liveBenefitTitle: "라이브 한정 추가 쿠폰 제공",
+          productName: "상품 이름",
+          productImgUrl: "https://g-selected.pstatic.net/MjAyNDA0MjJfNjMg/MDAxNzEzNzQyMDA2NjEx.UWvsFEHFzOoSUWwKGXJIl3T7tJrNRte4Bc39WLbc0gEg.5EipWVepAxebwdX8HRURM4kiI0phN1juuk3IkQVvyycg.JPEG/image.jpg?type=f320_480_q90",
+          productDescription: "상품 설명이 위치하는 영역입니다.",
+          productPrice: "60% 할인 123,450원",
+          shopName: "testshop",
+          shopThumbnailUrl: "https://phinf.pstatic.net/dthumb/?src=%22http%3A%2F%2Fshop1.phinf.naver.net%2F20220412_182%2F1649725677134MWG9L_PNG%2F50861519847511263_1050462988.png%22&service=selective&type=f180_180_q90",
+        },
+        {
+          liveTime: "09:10",
+          thumbnailUrl: "https://g-selected.pstatic.net/MjAyNDA0MjJfNjMg/MDAxNzEzNzQyMDA2NjEx.UWvsFEHFzOoSUWwKGXJIl3T7tJrNRte4Bc39WLbc0gEg.5EipWVepAxebwdX8HRURM4kiI0phN1juuk3IkQVvyycg.JPEG/image.jpg?type=f320_480_q90",
+          liveTitle: "live title test",
+          liveBenefitTitle: "라이브 한정 추가 쿠폰 제공",
+          productName: "상품 이름",
+          productImgUrl: "https://g-selected.pstatic.net/MjAyNDA0MjJfNjMg/MDAxNzEzNzQyMDA2NjEx.UWvsFEHFzOoSUWwKGXJIl3T7tJrNRte4Bc39WLbc0gEg.5EipWVepAxebwdX8HRURM4kiI0phN1juuk3IkQVvyycg.JPEG/image.jpg?type=f320_480_q90",
+          productDescription: "상품 설명이 위치하는 영역입니다.",
+          productPrice: "60% 할인 123,450원",
+          shopName: "testshop",
+          shopThumbnailUrl: "https://phinf.pstatic.net/dthumb/?src=%22http%3A%2F%2Fshop1.phinf.naver.net%2F20220412_182%2F1649725677134MWG9L_PNG%2F50861519847511263_1050462988.png%22&service=selective&type=f180_180_q90",
+        },
+      ],
+    };
   },
 };
 </script>
 <style scoped>
 .content-wrapper {
-  width: 100%;
-  padding-left:11.5%;
-  padding-right:11.5%;
+  width: 77%;
+  margin: auto;
   display:flex!important;
   flex-direction:column;
   justify-content: center;
   align-items: center;
+  border-left: 1px solid lightgray;
+  border-right: 1px solid lightgray;
 }
 .under-category-section {
   border-left: 1px solid #e6e7eb;
@@ -39,4 +118,9 @@ export default {
 .under-category-section::-webkit-scrollbar {
   display: none;
 }
+
+hr{
+  width:100%;
+}
+
 </style>
