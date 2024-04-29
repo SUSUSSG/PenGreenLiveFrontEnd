b8337d20562c2313c56d7dc84e55d771077fa59a
zzzjinwook <zzzjinwook@naver.com>
2024-04-29 19:50:52 +0900

    feat : 환경 설명 페이지추가
    여기에는 인증 라벨 이야기, 사이트 설명, 환경 이야기 등이 들어갑니다.

---
+++ src/views/main/environment.vue
@@ -0,0 +1,33 @@
+<template>
+  <MenuHeaderNav />
+  <hr/>
+  <div class="content-wrapper">
+    대충 환경이야기
+    우리 펭귄 집 없어요...
+  </div>
+</template>
+
+<script>
+import MenuHeaderNav from "@/components/HeaderMain/menu-header-nav.vue";
+export default {
+  components: {
+    MenuHeaderNav,
+  },
+};
+</script>
+<style scoped>
+.content-wrapper {
+  width: 77%;
+  margin: auto;
+  display: flex !important;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  border-left: 1px solid lightgray;
+  border-right: 1px solid lightgray;
+}
+
+hr {
+  width: 100%;
+}
+</style>
