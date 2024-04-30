7b032172817b47fad1387e31e085ac22de4ac190
zzzjinwook <zzzjinwook@naver.com>
2024-04-30 18:16:14 +0900

    fix : 통계 컴포넌트 디자인 수정

--- src/views/dashboard/product-statistics.vue
+++ src/views/dashboard/product-statistics.vue
@@ -1,13 +1,15 @@
 <template>
-  <div class="flex-row">
-    <AnalyticsCard
-      v-for="card in analyticsCards"
-      :key="card.id"
-      :icon="card.icon"
-      :analyticsTitle="card.title"
-      :analyticsResult="card.result"
-      style="width: 100%;"
-    />
+  <div class="content-wrapper">
+    <div class="flex-row p-4" style="gap: 10px">
+      <AnalyticsCard
+        v-for="card in analyticsCards"
+        :key="card.id"
+        :icon="card.icon"
+        :analyticsTitle="card.title"
+        :analyticsResult="card.result"
+        style="width: 100%"
+      />
+    </div>
   </div>
 </template>
 
@@ -23,42 +25,49 @@ export default {
       analyticsCards: [
         {
           id: 1,
-          icon: "uil-users",
-          title: "Total Users",
-          result: "92600",
+          icon: "uil-money-withdrawal",
+          title: "누적 거래액",
+          result: "92600000원",
         },
         {
           id: 2,
-          icon: "uil-graph-bar",
-          title: "Revenue",
-          result: "58600",
+          icon: "uil-money-bill",
+          title: "상품 평균 주문 수",
+          result: "58600건",
         },
         {
           id: 3,
-          icon: "uil-shopping-cart",
-          title: "Orders",
-          result: "14200",
+          icon: "uil-bag",
+          title: "상품 평균 판매 금액",
+          result: "49054980원",
         },
         {
           id: 4,
-          icon: "uil-comments",
-          title: "Comments",
-          result: "8220",
+          icon: "uil-user",
+          title: "상품 평균 구매자 수",
+          result: "4908명",
         },
         {
           id: 5,
-          icon: "uil-bag",
-          title: "Products",
-          result: "6785",
+          icon: "uil-shopping-cart",
+          title: "상품 평균 구매 개수",
+          result: "5215개",
         },
       ],
     };
   },
 };
 </script>
+
 <style scoped>
 .flex-row {
   display: flex;
   flex-direction: row;
+}
+
+.content-wrapper {
+  width: 100%;
+  height: 100%;
+  background-color: white;
 }
 </style>
