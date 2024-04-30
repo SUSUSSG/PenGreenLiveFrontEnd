67f40abbedc5ab303be0fea700058069bd384f86
zzzjinwook <zzzjinwook@naver.com>
2024-04-30 18:06:48 +0900

    feat : 컴포넌트 호출 로직

--- src/views/dashboard/product-statistics.vue
+++ src/views/dashboard/product-statistics.vue
@@ -1,12 +1,64 @@
 <template>
-    
+  <div class="flex-row">
+    <AnalyticsCard
+      v-for="card in analyticsCards"
+      :key="card.id"
+      :icon="card.icon"
+      :analyticsTitle="card.title"
+      :analyticsResult="card.result"
+      style="width: 100%;"
+    />
+  </div>
 </template>
+
 <script>
-import productlist from "@/components/Product/product-list.vue";
+import AnalyticsCard from "@/components/Card/analytics-card.vue";
+
 export default {
   components: {
-    productlist,
+    AnalyticsCard,
+  },
+  data() {
+    return {
+      analyticsCards: [
+        {
+          id: 1,
+          icon: "uil-users",
+          title: "Total Users",
+          result: "92600",
+        },
+        {
+          id: 2,
+          icon: "uil-graph-bar",
+          title: "Revenue",
+          result: "58600",
+        },
+        {
+          id: 3,
+          icon: "uil-shopping-cart",
+          title: "Orders",
+          result: "14200",
+        },
+        {
+          id: 4,
+          icon: "uil-comments",
+          title: "Comments",
+          result: "8220",
+        },
+        {
+          id: 5,
+          icon: "uil-bag",
+          title: "Products",
+          result: "6785",
+        },
+      ],
+    };
   },
 };
 </script>
-<style lang=""></style>
+<style scoped>
+.flex-row {
+  display: flex;
+  flex-direction: row;
+}
+</style>
