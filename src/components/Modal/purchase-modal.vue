3e0397cea8d334d583876ca1b639a8ea42428067
장서윤 <seoy316@naver.com>
2024-05-02 11:44:55 +0900

    feat: 상품 구매 모달 컴포넌트 생성 및 구매 수량 변경 구현

---
+++ src/components/Modal/purchase-modal.vue
@@ -0,0 +1,115 @@
+<template>
+  <div class="modal-content w-[100%] bg-white">
+    <div class="purchase-wrap">
+      <div class="modal-handle-area w-full h-[1rem] flex justify-center" @click="close">
+        <span class="modal-handle"></span>
+      </div>
+      <div>
+        동구밭 중건성 헤어케어 5종 기획세트
+      </div>
+      <div class="w-full flex justify-between items-center">
+        <div class="quantity-controls h-[30px]">
+          <button @click="decreaseQuantity" class="btn-control h-full">-</button>
+          <input type="text" v-model="quantity" class="input-number h-full" readonly/>
+          <button @click="increaseQuantity" class="btn-control h-full">+</button>
+        </div>
+        <div class="items-right">33,000원</div>
+      </div>
+      <div class="address-input w-full flex justify-between items-center">
+        <label for="address">서울특별시 관악구 관악로12길 16-37</label>
+        <button>변경</button>
+      </div>
+    </div>
+    <button class="order-button">눌러서 구매하기</button>
+  </div>
+</template>
+
+<script setup>
+import { ref, defineEmits } from 'vue';
+
+const emit = defineEmits(['update:isOpen']);
+const close = () => {
+  emit('update:isOpen', false);
+};
+
+const props = defineProps({
+  isOpen: Boolean
+});
+
+const quantity = ref(1);
+
+const increaseQuantity = () => {
+  quantity.value++;
+};
+
+const decreaseQuantity = () => {
+  if (quantity.value > 1) {
+    quantity.value--;
+  }
+};
+
+</script>
+
+<style scoped>
+.modal-handle {
+  display: block;
+  width: 40px;
+  height: 6px;
+  background-color: #999;
+  border-radius: 3px;
+}
+
+.number-input {
+  display: flex;
+  align-items: center;
+  border: 1px solid #333;
+  border-radius: 0;
+  color: black;
+}
+
+.btn-control {
+  border: 1px solid #333;
+  cursor: pointer;
+  user-select: none;
+  border-radius: 0; /* 각진 버튼 디자인 */
+  transition: background-color 0.3s ease;
+}
+
+.btn-control:hover {
+  background-color: #ccc;
+}
+
+.input-number {
+  text-align: center;
+  border: none;
+  border-top: 1px solid #333;
+  border-bottom: 1px solid #333;
+  font-size: 15px;
+  background-color: white;
+  color: black;
+}
+
+.quantity-controls button {
+  width: 30px;
+  height: 30px;
+}
+
+.quantity-controls input {
+  text-align: center;
+  width: 50px;
+}
+
+.address-input {
+  margin-top: 10px;
+}
+
+.order-button {
+  width: 100%;
+  background-color: black;
+  color: white;
+  padding: 10px 0;
+  border: none;
+  cursor: pointer;
+}
+
+</style>
