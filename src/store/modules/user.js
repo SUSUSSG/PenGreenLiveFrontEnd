b491030f837ff48ab71fafac581e2afab5118cff
zzzjinwook <zzzjinwook@naver.com>
2024-05-08 20:37:33 +0900

    feat : 방 DB에서 가져오기

--- src/store/modules/user.js
+++ src/store/modules/user.js
@@ -19,18 +19,54 @@ const state = {
   };
   
   const actions = {
-    loadRooms({ commit }) {
-      commit('SET_ROOMS', [
-        { id: 1, name: 'Room 1' },
-        { id: 2, name: 'Room 2' },
-        { id: 3, name: 'Room 3' },
-      ]);
-    },
-    loadEnteredRooms({ commit }) {
-      commit('SET_ENTERED_ROOMS', [
-        { id: 1, name: 'Room 1' },
-      ]);
-    },
+    async loadRooms(context) {
+
+      let response = await fetch(`/api/v1/room`, {
+          method: "GET"
+      });
+
+      const responseData = await response.json();
+
+      if(!response.ok) {
+          //error }
+      }
+
+      const rooms = [];
+      for(const data of responseData.data) {
+          const room = {
+              id: data.roomId,
+              name: data.name
+          };
+
+          rooms.push(room);
+      }
+
+      context.commit('setRooms', rooms);
+  },
+
+  async loadEnteredRooms(context) {
+      const userId = 2;
+
+      let response = await fetch(`/api/v1/room/joined?userId=${userId}`, {
+          method: "GET",
+      });
+
+      const responseData = await response.json();
+      if(!response.ok) {
+          //error }
+      }
+      const enteredRooms = [];
+      for(const data of responseData.data) {
+          const room = {
+              id: data.roomId,
+              name: data.roomName
+          };
+          enteredRooms.push(room);
+      }
+
+      context.commit('setEnteredRooms', enteredRooms);
+
+  }
   };
   
   const getters = {
