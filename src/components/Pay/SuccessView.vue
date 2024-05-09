6928d2b43a7326c4741515468dd858968eee19d4
장서윤 <seoy316@naver.com>
2024-05-09 17:42:09 +0900

    css: 결제 성공, 실패 페이지 디자인

--- src/components/Pay/SuccessView.vue
+++ src/components/Pay/SuccessView.vue
@@ -1,40 +1,35 @@
 <template>
-    <!-- 결제 성공 시 -->
-    <section v-if="confirmed">
-      <div class="box_section" style="width: 600px">
-        <img style="width: 100px" src="https://static.toss.im/illusts/check-blue-spot-ending-frame.png" />
-        <h2>결제를 완료했어요</h2>
-  
+  <!-- 결제 성공 시 -->
+  <section class="h-[80vh] flex flex-col justify-center"> <!-- v-if="confirmed"> -->
+    <div class="box_section py-[10rem] flex flex-col " style="width: 550px">
+      <div class="w-full pb-[1rem] flex justify-center items-stretchs">
+        <img style="width: 75px" src="https://static.toss.im/illusts/check-blue-spot-ending-frame.png" />
+      </div>
+      <h6 class="w-full">결제를 완료했어요</h6>
+
+      <div class="w-full flex flex-col">
         <div class="p-grid typography--p" style="margin-top: 50px">
           <div class="p-grid-col text--left"><b>결제금액</b></div>
-          <div class="p-grid-col text--right" id="amount">{{ jsonData.totalAmount }}원</div>
+          <div class="p-grid-col text--right" id="amount">{{ totalPrice }}원</div>
         </div>
         <div class="p-grid typography--p" style="margin-top: 10px">
           <div class="p-grid-col text--left"><b>주문번호</b></div>
           <div class="p-grid-col text--right" id="orderId">{{ jsonData.orderId }}</div>
         </div>
-        <div class="p-grid typography--p" style="margin-top: 10px">
-          <div class="p-grid-col text--left"><b>paymentKey</b></div>
-          <div class="p-grid-col text--right" id="paymentKey" style="white-space: initial; width: 250px">{{ jsonData.paymentKey }}</div>
         </div>
-        <div class="p-grid" style="margin-top: 30px">
-          <button class="button p-grid-col5" onclick="location.href='https://docs.tosspayments.com/guides/payment/integration';">연동 문서</button>
-          <button class="button p-grid-col5" onclick="location.href='https://discord.gg/A4fRFXQhRu';" style="background-color: #e8f3ff; color: #1b64da">실시간 문의</button>
-        </div>
+      <div class="w-full mt-10">
+        <Button>돌아가기</Button>
       </div>
-      <div class="box_section" style="width: 600px; text-align: left">
-        <b>Response Data :</b>
-        <div id="response" style="white-space: initial">
-          <pre>{{ jsonData }}</pre>
-        </div>
-      </div>
-    </section>
-  </template>
+    </div>
+  </section>
+</template>
   
-  <script>
+<script setup>
   import { ref, onMounted } from "vue";
   import { useRoute, useRouter } from "vue-router";
-  import { confirmPayment } from "@/confirmPayment";
+  import { confirmPayment } from "@/constant/confirmPayment";
+  import Button from "@/components/Button";
+  import "@/components/Pay/style.css";
   
   export default {
     setup() {
@@ -42,6 +37,7 @@
       const router = useRouter();
       const confirmed = ref(false);
       const jsonData = ref(null);
+      const totalPrice = ref(null);
   
       onMounted(async () => {
         const requestData = {
@@ -59,17 +55,20 @@
             } else {
               confirmed.value = true;
               jsonData.value = json;
+              totalPrice.value = json.totalAmount.toLocaleString();
+
+          
             }
           } catch (error) {}
         }
-  
         confirm();
       });
   
       return {
         confirmed,
         jsonData,
+        totalPrice,
       };
     },
   };
-  </script>
+</script> 
