432562a3d632a3a9141fe8618f8767991a279ee6
SonHyeJi <kuy06046@naver.com>
2024-05-11 02:41:48 +0900

    feat : 상점정보 조회 및 수정 기능 구현

--- src/components/shop/shop-modify.vue
+++ src/components/shop/shop-modify.vue
@@ -1,16 +1,17 @@
 <template>
   <div class="space-y-3 shopinfo">
-    <Textinput label="상점 닉네임" name="nickname" type="text" placeholder="닉네임을 입력하세요. " />
-    <Textinput label="자사몰 링크" name="shoplink" type="text" placeholder="www.example.com" />
+    <Textinput label="상점 닉네임" name="nickname" v-model="shopInfo.nickname" type="text" placeholder="닉네임을 입력하세요. " />
+    <Textinput label="자사몰 링크" name="shoplink" v-model="shopInfo.shoplink" type="text" placeholder="www.example.com" />
     <label class="ltr:inline-block rtl:block input-label">대표이미지 등록</label><br>
     <input type="file" id="imageUpload" @change="handleImageUpload" accept="image/jpeg, image/png, image/gif"
       class="mt-2 text-base text-slate-600 dark:text-slate-300" />
     <img v-if="previewImage" :src="previewImage" alt="대표 이미지 미리보기" style="max-width: 100px">
 
-    <Textarea label="상점 소개글" name="description" placeholder="상점을 소개해보세요" />
+    <Textarea label="상점 소개글" name="description" v-model="shopInfo.description" placeholder="상점을 소개해보세요" />
 
     <div class="text-center">
-      <button class="btn inline-flex justify-center btn-sm btn-dark mr-3 mt-3"><span>저장</span></button>
+      <button class="btn inline-flex justify-center btn-sm btn-dark mr-3 mt-3"
+        @click="saveShopInfo"><span>저장</span></button>
       <button class="btn inline-flex justify-center btn-sm  btn-dark" @click="resetForm"><span>취소</span></button>
     </div>
   </div>
@@ -19,6 +20,7 @@
 </template>
 
 <script>
+import axios from 'axios';
 import Textinput from "@/components/Textinput";
 import Textarea from "@/components/Textarea";
 import Select from "@/components/Select";
@@ -31,7 +33,16 @@ export default {
   data() {
     return {
       previewImage: null,
+      shopInfo: {
+        channelSeq: 1,
+        shoplink: '',
+        description: '',
+        image: null,
+      }
     };
+  },
+  created() {
+    this.loadShopInfo();
   },
   methods: {
     handleImageUpload(event) {
@@ -40,11 +51,60 @@ export default {
         const reader = new FileReader();
         reader.onload = (e) => {
           this.previewImage = e.target.result;
+          this.shopInfo.image = file;
         };
         reader.readAsDataURL(file);
       } else {
         this.previewImage = null;
       }
+    },
+    getChannelSeqFromUrl() {
+      // URL에서 채널 ID를 추출하는 로직 (여기서는 간단히 예시로 구현)
+      const path = window.location.pathname;
+      return path.split('/').pop();
+    },
+    loadShopInfo() {
+  const url = `http://localhost:8090/shop/${this.shopInfo.channelSeq}`;
+  axios.get(url)
+    .then(response => {
+      this.shopInfo.nickname = response.data.channelNM;
+      this.shopInfo.shoplink = response.data.channelUrl;
+      this.shopInfo.description = response.data.channelInfo;
+
+      // 이미지 데이터 처리를 위해 별도의 요청을 보내거나, 적절한 응답 처리 필요
+      if (response.data.channelImage) {
+        // 예를 들어, 이미지가 base64 문자열로 저장되어 있을 경우 직접 할당
+        this.previewImage = `data:image/jpeg;base64,${response.data.channelImage}`;
+      }
+    })
+    .catch(error => {
+      console.error('상점 정보 로드 실패:', error);
+      alert('상점 정보 로드 중 오류가 발생했습니다.');
+    });
+},
+
+
+    saveShopInfo() {
+      const formData = new FormData();
+      formData.append('nickname', this.shopInfo.nickname);
+      formData.append('shoplink', this.shopInfo.shoplink);
+      formData.append('description', this.shopInfo.description);
+      formData.append('image', this.shopInfo.image);
+
+      const url = `http://localhost:8090/shop-modify/${this.shopInfo.channelSeq}`;
+
+      axios.put(url, formData, {
+        headers: {
+          'Content-Type': 'multipart/form-data'
+        }
+      })
+        .then(response => {
+          alert('상점 정보가 성공적으로 업데이트되었습니다.');
+        })
+        .catch(error => {
+          console.error('상점 정보 업데이트 실패:', error);
+          alert('상점 정보 업데이트 중 오류가 발생했습니다.');
+        });
     },
     resetForm() {
 
