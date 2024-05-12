4236ba665fd03bf097e2cbc7d27230f01e87075a
hinata7010 <1234minseok@naver.com>
2024-05-12 15:26:10 +0900

    feat : StreamManager Props 추가 및 방송화면 송출하도록 추가

--- src/components/LiveBoard/liveboard-broad.vue
+++ src/components/LiveBoard/liveboard-broad.vue
@@ -1,46 +1,50 @@
 <template>
-    <div class="bg-white" id="broadCard">
-        <div class="text-base">
-            송출 화면
-        </div>
-        <videoPlayer :videoSource="videoSourceSrc" class="video-player" />
+  <div class="bg-white" id="broadCard">
+    <div class="text-base">
+      송출 화면
     </div>
+    <div v-if="streamManager" class="video-player">
+      <VideoPlayer :stream-manager="streamManager"/>
+    </div>
+  </div>
 </template>
 
 <script>
-import videoPlayer from "@/components/Video/videoplayer.vue";
-import videoSource from "@/assets/video/test-video.mp4";
+import VideoPlayer from "@/components/Video/videoplayer.vue";
+import OvVideo from "@/views/test/OvVideo.vue";
 
 export default {
-    components: {
-        videoPlayer
-    },
-    data() {
-        return {
-            videoSourceSrc: videoSource
-        }
-    }
+  components: {
+    OvVideo,
+    VideoPlayer,
+  },
+  props: {
+    streamManager: Object,
+  },
+  data() {
+    return {}
+  }
 }
 </script>
 
 <style scoped>
 #broadCard {
-    height: 100%;
-    width: auto;
-    display: flex;
-    flex-direction: column;
-    background: white;
-    box-shadow: 0 0.25rem 0.5rem rgba(0, 0, 0, 0.1);
+  height: 100%;
+  width: auto;
+  display: flex;
+  flex-direction: column;
+  background: white;
+  box-shadow: 0 0.25rem 0.5rem rgba(0, 0, 0, 0.1);
 }
 
 .text-base {
-    margin-bottom: 5px;
+  margin-bottom: 5px;
 }
 
 .video-player {
-    aspect-ratio: 9/16;
-    width:100%;
-    object-fit:cover;
+  aspect-ratio: 9/16;
+  width: 100%;
+  object-fit: cover;
 }
 
 </style>
--- src/components/Video/videoplayer.vue
+++ src/components/Video/videoplayer.vue
@@ -1,36 +1,22 @@
 <template>
   <div class="video-wrapper">
-    <video ref="videoPlayer" class="video" playsinline>
-      <source :src="videoSource" type="video/mp4">
-      Your browser does not support the video tag.
-    </video>
+    <!-- ref 속성을 추가하여 video 요소를 직접 참조할 수 있습니다. -->
+    <video ref="videoElement" autoPlay class="video"></video>
   </div>
 </template>
 
 <script>
 export default {
+  name: 'VideoPlayer',
+
   props: {
-    videoSource: {
-      type: String,
-      required: true
-    }
+    streamManager: Object,
   },
+
   mounted() {
-    this.initializePlayer();
+    // this.$refs를 통해 video 요소에 접근하고, 해당 요소에 비디오 스트림을 추가합니다.
+    this.streamManager.addVideoElement(this.$refs.videoElement);
   },
-  methods: {
-    initializePlayer() {
-      const videoElement = this.$refs.videoPlayer;
-      videoElement.muted = true; // Start the video muted
-      videoElement.play()
-          .then(() => {
-            console.log("Video playback started!");
-          })
-          .catch((error) => {
-            console.error("Error starting video playback:", error);
-          });
-    }
-  }
 };
 </script>
 
@@ -40,8 +26,7 @@ export default {
   aspect-ratio: 9 / 16;
 }
 
-.video{
-  /* max-height: 906px; */
+.video {
   width: 100%;
   height: auto;
   display: block;
