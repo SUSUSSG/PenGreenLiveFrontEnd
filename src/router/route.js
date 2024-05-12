7eac9d70210fb41b4e8155b49e4d301b333b68fa
hinata7010 <1234minseok@naver.com>
2024-05-12 15:25:38 +0900

    feat : 방송 시청 기능 추가 및 동적 URL 추가

--- src/router/route.js
+++ src/router/route.js
@@ -12,7 +12,11 @@ const routes = [
       { path: 'member/login', name: '일반회원로그인', component: () => import('@/views/user/login.vue') },
       { path: 'enterprise/join', name: '판매자로그인', component: () => import('@/views/enterprise/join.vue') },
       { path: 'login', name: '로그인', component: () => import('@/views/enterprise/login.vue') },
-      { path: 'live-viewing', name: '라이브시청', component: () => import('@/views/broadcast/live-viewing.vue') },
+      {
+        path: '/live-viewing/:broadcastId',
+        name: 'LiveViewing',
+        component: () => import('@/views/broadcast/live-viewing.vue')
+      },
       { path: 'test', name: '테스트', component: () => import('@/views/test/test-openvidu.vue')},
       { path: 'test-viewer', name: '테스트시청자', component: () => import('@/views/test/test-viewer.vue')}
     ],
