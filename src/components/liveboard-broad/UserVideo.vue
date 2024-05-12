54ef1de26b4287991d404dfd154b291e053a76b6
hinata7010 <1234minseok@naver.com>
2024-05-12 15:40:54 +0900

    file : 파일명 변경

---
+++ src/components/liveboard-broad/UserVideo.vue
@@ -0,0 +1,50 @@
+<template>
+  <div class="bg-white" id="broadCard">
+    <div class="text-base">
+      송출 화면
+    </div>
+    <div v-if="streamManager">
+      <VideoPlayer :stream-manager="streamManager" class="video-player"/>
+    </div>
+  </div>
+</template>
+
+<script>
+import VideoPlayer from "@/components/Video/videoplayer.vue";
+import OvVideo from "@/views/test/OvVideo.vue";
+
+export default {
+  components: {
+    OvVideo,
+    VideoPlayer,
+  },
+  props: {
+    streamManager: Object,
+  },
+  data() {
+    return {}
+  }
+}
+</script>
+
+<style scoped>
+#broadCard {
+  height: 100%;
+  width: auto;
+  display: flex;
+  flex-direction: column;
+  background: white;
+  box-shadow: 0 0.25rem 0.5rem rgba(0, 0, 0, 0.1);
+}
+
+.text-base {
+  margin-bottom: 5px;
+}
+
+.video-player {
+  aspect-ratio: 9/16;
+  width: 100%;
+  object-fit: cover;
+}
+
+</style>
