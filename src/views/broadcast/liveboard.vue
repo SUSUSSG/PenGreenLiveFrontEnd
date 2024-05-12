08874ef90855cc2f4b1756c6c766a70498fa1bd4
hinata7010 <1234minseok@naver.com>
2024-05-12 15:25:48 +0900

    feat : 방송 송출 기능 추가

--- src/views/broadcast/liveboard.vue
+++ src/views/broadcast/liveboard.vue
@@ -1,11 +1,11 @@
 <template>
   <div class="main-wrapper">
-    <LiveBoardTime />
+    <LiveBoardTime @start-broadcast="joinSession" @stop-broadcast="leaveSession"/>
     <div class="content-wrapper">
       <div class="flex-row">
         <div class="flex-col">
           <div class="flex-row">
-            <LiveboardBroad />
+            <LiveboardBroad :stream-manager="mainStreamManager"/>
             <LiveBoardChat :current-room="{ id: 1 }" :current-writer="'판매자'"/>
           </div>
         </div>
@@ -22,26 +22,177 @@
   </div>
 </template>
 <script>
+import { OpenVidu } from "openvidu-browser";
+import axios from "axios";
 import LiveBoardTime from "@/components/LiveBoard/liveboard-time.vue";
 import LiveBoardChat from "@/components/LiveBoard/liveboard-chat.vue";
-import LiveboardBroad from "@/components/LiveBoard/liveboard-broad.vue";
+import LiveboardBroad from "@/components/liveboard/liveboard-broad.vue";
 import LiveBoardStatistics from "@/components/LiveBoard/liveboard-statistics.vue";
 import LiveboardPrompt from "@/components/LiveBoard/liveboard-prompt.vue";
 import LiveboardSidebar from "@/components/LiveBoard/liveboard-sidebar.vue";
 import LiveboardProduct from "@/components/LiveBoard/liveboard-product.vue";
+import UserVideo from "@/components/liveboard-broad/UserVideo.vue";
+import VideoPlayer from "@/components/Video/videoplayer.vue";
+import Live from "@/components/Video/live.vue"
+
+const APPLICATION_SERVER_URL = process.env.NODE_ENV === 'production' ? '' : 'http://localhost:8090/';
+axios.defaults.headers.post["Content-Type"] = "application/json";
 
 export default {
   components: {
+    VideoPlayer,
+    UserVideo,
     LiveBoardTime,
     LiveBoardChat,
     LiveboardBroad,
     LiveBoardStatistics,
     LiveboardPrompt,
     LiveboardSidebar,
-    LiveboardProduct
-  }
+    LiveboardProduct,
+    Live
+  },
+  data() {
+    return {
+      // OpenVidu objects
+      OV: undefined,
+      session: undefined,
+      mainStreamManager: undefined,
+      publisher: undefined,
+      subscribers: [],
+
+      // Join form
+      mySessionId: "1",
+      myUserName: "Admin",
+    };
+  },
+
+  methods: {
+    joinSession() {
+      // --- 1) Get an OpenVidu object ---
+      this.OV = new OpenVidu();
+
+      // --- 2) Init a session ---
+      this.session = this.OV.initSession();
+
+      // --- 3) Specify the actions when events take place in the session ---
+
+      // On every new Stream received...
+      this.session.on("streamCreated", ({ stream }) => {
+        const subscriber = this.session.subscribe(stream);
+        this.subscribers.push(subscriber);
+      });
+
+      // On every Stream destroyed...
+      this.session.on("streamDestroyed", ({ stream }) => {
+        const index = this.subscribers.indexOf(stream.streamManager, 0);
+        if (index >= 0) {
+          this.subscribers.splice(index, 1);
+        }
+      });
+
+      // On every asynchronous exception...
+      this.session.on("exception", ({ exception }) => {
+        console.warn(exception);
+      });
+
+      // --- 4) Connect to the session with a valid user token ---
+
+      // Get a token from the OpenVidu deployment
+      this.getToken(this.mySessionId).then((token) => {
+
+        // First param is the token. Second param can be retrieved by every user on event
+        // 'streamCreated' (property Stream.connection.data), and will be appended to DOM as the user's nickname
+        this.session.connect(token, { clientData: this.myUserName })
+            .then(() => {
+
+              // --- 5) Get your own camera stream with the desired properties ---
+
+              // Init a publisher passing undefined as targetElement (we don't want OpenVidu to insert a video
+              // element: we will manage it on our own) and with the desired properties
+              let publisher = this.OV.initPublisher(undefined, {
+                audioSource: undefined, // The source of audio. If undefined default microphone
+                videoSource: undefined, // The source of video. If undefined default webcam
+                publishAudio: true, // Whether you want to start publishing with your audio unmuted or not
+                publishVideo: true, // Whether you want to start publishing with your video enabled or not
+                resolution: "900x1600", // The resolution of your video
+                frameRate: 30, // The frame rate of your video
+                insertMode: "APPEND", // How the video is inserted in the target element 'video-container'
+                mirror: false, // Whether to mirror your local video or not
+              });
+
+              // Set the main video in the page to display our webcam and store our Publisher
+              this.mainStreamManager = publisher;
+              this.publisher = publisher;
+
+              // --- 6) Publish your stream ---
+
+              this.session.publish(this.publisher);
+            })
+            .catch((error) => {
+              console.log("There was an error connecting to the session:", error.code, error.message);
+            });
+      });
+
+      window.addEventListener("beforeunload", this.leaveSession);
+    },
+
+    leaveSession() {
+      // --- 7) Leave the session by calling 'disconnect' method over the Session object ---
+      if (this.session) this.session.disconnect();
+
+      // Empty all properties...
+      this.session = undefined;
+      this.mainStreamManager = undefined;
+      this.publisher = undefined;
+      this.subscribers = [];
+      this.OV = undefined;
+
+      // Remove beforeunload listener
+      window.removeEventListener("beforeunload", this.leaveSession);
+    },
+
+    updateMainVideoStreamManager(stream) {
+      if (this.mainStreamManager === stream) return;
+      this.mainStreamManager = stream;
+    },
+
+    /**
+     * --------------------------------------------
+     * GETTING A TOKEN FROM YOUR APPLICATION SERVER
+     * --------------------------------------------
+     * The methods below request the creation of a Session and a Token to
+     * your application server. This keeps your OpenVidu deployment secure.
+     *
+     * In this sample code, there is no user control at all. Anybody could
+     * access your application server endpoints! In a real production
+     * environment, your application server must identify the user to allow
+     * access to the endpoints.
+     *
+     * Visit https://docs.openvidu.io/en/stable/application-server to learn
+     * more about the integration of OpenVidu in your application server.
+     */
+    async getToken(mySessionId) {
+      const sessionId = await this.createSession(mySessionId);
+      return await this.createToken(sessionId);
+    },
+
+    async createSession(sessionId) {
+      const response = await axios.post(APPLICATION_SERVER_URL + 'api/sessions', { customSessionId: sessionId }, {
+        headers: { 'Content-Type': 'application/json', },
+      });
+      return response.data; // The sessionId
+    },
+
+    async createToken(sessionId) {
+      const response = await axios.post(APPLICATION_SERVER_URL + 'api/sessions/' + sessionId + '/connections', {}, {
+        headers: { 'Content-Type': 'application/json', },
+      });
+      return response.data; // The token
+    },
+  },
 };
 </script>
+
 <style lang="scss" scoped>
 .main-wrapper{
   width:100%;
