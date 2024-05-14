f7e0965d5eebebc6178cfdf0081a1549bc546a03
JiniiW <ksjine2@gmail.com>
2024-05-15 03:20:14 +0900

    feat : 백엔드 연결 구현

--- src/views/dashboard/live-prepare.vue
+++ src/views/dashboard/live-prepare.vue
@@ -7,7 +7,7 @@
         :thumbimage-src="item.thumbimageSrc"
     :product-image-src="item.productimageSrc"
     :product-name="item.productName"
-    :product-price="item.productPrice"
+    :product-price="item.originalPrice"
     :discount-rate="item.discountRate"
     :live-date-time="item.liveDateTime"
     />
@@ -16,7 +16,7 @@
 
 <script>
 import SellerBroadcastSchedule from "@/components/Card/seller-broadcast-schedule.vue";
-import noImage from "@/assets/images/all-img/no-image.png" // Verify this path is correct
+import axios from "axios";
 
 export default {
   name: "live-prepare",
@@ -27,72 +27,46 @@ export default {
     let liveTime = new Date();
     liveTime.setMinutes(liveTime.getMinutes() + 15);  // 현재 시간으로부터 15분 후
     return {
+      boradcastSeq: '',
       items: [
         {
-          "id": 1,
-          "broadcastTitle": "맛있는 사과를 팔아요",
-          "thumbimageSrc": "http://via.placeholder.com/200x200",
-          "productimageSrc": "http://via.placeholder.com/100x130",
-          "productName": "충주 사과",
-          "productPrice": 42000,
-          "discountRate": 30,
-          "liveDateTime": liveTime.toISOString()
-        },
-        {
-          "id": 2,
-          "broadcastTitle": "신선한 오렌지 판매중",
-          "thumbimageSrc": "http://via.placeholder.com/200x200",
-          "productimageSrc": "http://via.placeholder.com/100x130",
-          "productName": "제주 오렌지",
-          "productPrice": 35000,
-          "discountRate": 20,
-          "liveDateTime": "2024-03-22-20:00"
-        },
-        {
-          "id": 3,
-          "broadcastTitle": "고소한 피칸 구매하세요",
-          "thumbimageSrc": "http://via.placeholder.com/200x200",
-          "productimageSrc": "http://via.placeholder.com/100x130",
-          "productName": "미국 피칸",
-          "productPrice": 50000,
-          "discountRate": 25,
-          "liveDateTime": "2024-03-23-19:30"
-        },
-        {
-          "id": 4,
-          "broadcastTitle": "달콤한 체리, 지금 할인 중",
-          "thumbimageSrc": "http://via.placeholder.com/200x200",
-          "productimageSrc": "http://via.placeholder.com/100x130",
-          "productName": "워싱턴 체리",
-          "productPrice": 60000,
-          "discountRate": 15,
-          "liveDateTime": "2024-03-24-18:00"
-        },
-        {
-          "id": 4,
-          "broadcastTitle": "달콤한 체리, 지금 할인 중",
-          "thumbimageSrc": "http://via.placeholder.com/200x200",
-          "productimageSrc": "http://via.placeholder.com/100x130",
-          "productName": "워싱턴 체리",
-          "productPrice": 60000,
-          "discountRate": 15,
-          "liveDateTime": "2024-03-24-18:00"
-        },
-        {
-          "id": 4,
-          "broadcastTitle": "달콤한 체리, 지금 할인 중",
-          "thumbimageSrc": "http://via.placeholder.com/200x200",
-          "productimageSrc": "http://via.placeholder.com/100x130",
-          "productName": "워싱턴 체리",
-          "productPrice": 60000,
-          "discountRate": 15,
-          "liveDateTime": "2024-03-24-18:00"
-        },
-
-
+          broadcastTitle: '',
+          thumbimageSrc: '',
+          productImageSrc: '',
+          productName: '',
+          originalPrice: '',
+          discountRate: '',
+          liveDateTime: '',
+        }
       ]
-
     };
+  },
+  created() {
+    this.loadUpcomingBroadcastInfo();
+  },
+  methods: {
+    loadUpcomingBroadcastInfo() {
+      const url = `http://localhost:8090/upcoming-broadcasts`;
+
+      axios.get(url)
+        .then(response => {
+          console.log(response.data),
+          this.items = response.data.map (item => {
+            return {
+              broadcastTitle: item.broadcastTitle,
+              thumbimageSrc: item.broadcastImage,
+              productImageSrc: item.productImage,
+              productName: item.productNm,
+              originalPrice: item.listPrice,
+              discountRate: item.discountRate,
+              liveDateTime: item.broadcastScheduledTime,
+            }
+          });
+        })
+        .catch(error => {
+          console.error('방송 예정 목록 load 실패 : ', error);
+        })
+    }
   }
 }
 </script>
