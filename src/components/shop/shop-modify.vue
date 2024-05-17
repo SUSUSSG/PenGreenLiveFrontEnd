c939e23a04006511c13187f6a51c5ae434842df2
SonHyeJi <kuy06046@naver.com>
2024-05-18 00:46:10 +0900

    refactor : 상점 정보 이미지 로직 수정

--- src/components/shop/shop-modify.vue
+++ src/components/shop/shop-modify.vue
@@ -1,43 +1,39 @@
 <template>
   <div class="space-y-3 shopinfo">
-    <Textinput label="상점 닉네임" name="nickname" v-model="shopInfo.nickname" type="text" placeholder="닉네임을 입력하세요. " />
-    <Textinput label="자사몰 링크" name="shoplink" v-model="shopInfo.shoplink" type="text" placeholder="www.example.com" />
+    <Textinput label="상점 닉네임" name="nickname" v-model="shopInfo.channelNM" type="text" placeholder="닉네임을 입력하세요." />
+    <Textinput label="자사몰 링크" name="shoplink" v-model="shopInfo.channelUrl" type="text" placeholder="www.example.com" />
     <label class="ltr:inline-block rtl:block input-label">대표이미지 등록</label><br>
     <input type="file" id="imageUpload" @change="handleImageUpload" accept="image/jpeg, image/png, image/gif"
       class="mt-2 text-base text-slate-600 dark:text-slate-300" />
     <img v-if="previewImage" :src="previewImage" alt="대표 이미지 미리보기" style="max-width: 100px">
-
-    <Textarea label="상점 소개글" name="description" v-model="shopInfo.description" placeholder="상점을 소개해보세요" />
-
+    <Textarea label="상점 소개글" name="description" v-model="shopInfo.channelInfo" placeholder="상점을 소개해보세요" />
     <div class="text-center">
-      <button class="btn inline-flex justify-center btn-sm btn-dark mr-3 mt-3"
-        @click="saveShopInfo"><span>저장</span></button>
-      <button class="btn inline-flex justify-center btn-sm  btn-dark" @click="resetForm"><span>취소</span></button>
+      <button class="btn inline-flex justify-center btn-sm btn-dark mr-3 mt-3" @click="saveShopInfo"><span>저장</span></button>
+      <button class="btn inline-flex justify-center btn-sm btn-dark" @click="resetForm"><span>취소</span></button>
     </div>
   </div>
-
-
 </template>
 
 <script>
 import axios from 'axios';
 import Textinput from "@/components/Textinput";
 import Textarea from "@/components/Textarea";
-import Select from "@/components/Select";
+
 export default {
   components: {
     Textinput,
     Textarea,
-    Select,
   },
   data() {
     return {
       previewImage: null,
       shopInfo: {
-        channelSeq: 1,
-        shoplink: '',
-        description: '',
-        image: null,
+        channelSeq: 4,
+        channelNM: '',
+        channelUrl: '',
+        channelImage: null,
+        rawchannelImage: '',
+        channelInfo: '',
       }
     };
   },
@@ -46,12 +42,28 @@ export default {
   },
   methods: {
     handleImageUpload(event) {
+  const file = event.target.files[0];
+  if (file) {
+    
+    const reader = new FileReader();
+    reader.onload = (e) => { 
+      this.previewImage = e.target.result;
+      const base64String = e.target.result.split(',')[1]; // MIME 타입 정보 제거
+      this.imageSrc = base64String;
+    };
+    reader.readAsDataURL(file);
+  } else {
+    this.previewImage = null;
+    this.imageSrc = null;
+  }
+},
+    handleImageUpload(event) {
       const file = event.target.files[0];
       if (file) {
         const reader = new FileReader();
         reader.onload = (e) => {
           this.previewImage = e.target.result;
-          this.shopInfo.image = file;
+          this.shopInfo.channelImage = e.target.result.split(',')[1]; // 이미지를 Base64로 인코딩
         };
         reader.readAsDataURL(file);
       } else {
@@ -66,14 +78,14 @@ export default {
       const url = `http://localhost:8090/shop/${this.shopInfo.channelSeq}`;
       axios.get(url)
         .then(response => {
-          this.shopInfo.nickname = response.data.channelNM;
-          this.shopInfo.shoplink = response.data.channelUrl;
-          this.shopInfo.description = response.data.channelInfo;
+          this.shopInfo.channelNM = response.data.channelNM;
+          this.shopInfo.channelUrl = response.data.channelUrl;
+          this.shopInfo.channelInfo = response.data.channelInfo;
 
-          
-          if (response.data.channelImage) {
-            this.previewImage = `data:image/jpeg;base64,${response.data.channelImage}`;
-            this.shopInfo.image = response.data.channelImage;  
+          if (response.data.rawchannelImage) {
+            this.previewImage = response.data.rawchannelImage; // URL로부터 로드된 이미지
+            this.shopInfo.rawchannelImage = response.data.rawchannelImage;
+            this.shopInfo.channelImage = null; // URL에서 로드했으므로 base64 이미지를 초기화
           }
         })
         .catch(error => {
@@ -81,41 +93,33 @@ export default {
           alert('상점 정보 로드 중 오류가 발생했습니다.');
         });
     },
-
     saveShopInfo() {
-      const formData = new FormData();
-      formData.append('nickname', this.shopInfo.nickname);
-      formData.append('shoplink', this.shopInfo.shoplink);
-      formData.append('description', this.shopInfo.description);
-
-      if (this.shopInfo.image instanceof File) {
-        formData.append('image', this.shopInfo.image);
-      }
-
       const url = `http://localhost:8090/shop/${this.shopInfo.channelSeq}`;
-
-      axios.put(url, formData, {
+      const shopInfoToSend = {
+        ...this.shopInfo,
+        channelImage: this.shopInfo.channelImage || null, // base64 이미지가 있으면 보냄
+      };
+      axios.put(url, shopInfoToSend, {
         headers: {
-          'Content-Type': 'multipart/form-data'
+          'Content-Type': 'application/json'
         }
       })
         .then(response => {
           alert('상점 정보가 성공적으로 업데이트되었습니다.');
+          this.loadShopInfo(); // 업데이트 후 상점 정보 다시 로드
         })
         .catch(error => {
           console.error('상점 정보 업데이트 실패:', error);
           alert('상점 정보 업데이트 중 오류가 발생했습니다.');
         });
     },
-
     resetForm() {
-
-      this.previewImage = null;
-
+      this.loadShopInfo(); // 원래 상점 정보를 다시 로드
     }
   },
 };
 </script>
+
 <style lang="scss" scoped>
 .shopinfo {
   background-color: white;
