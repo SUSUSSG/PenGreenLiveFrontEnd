661e6309df356ad5d3044d2c23e7b9da497b40e5
JiniiW <ksjine2@gmail.com>
2024-05-19 13:10:39 +0900

    css : 방송 이미지 css 수정

--- src/components/LiveBoard/liveboard-broad.vue
+++ src/components/LiveBoard/liveboard-broad.vue
@@ -4,7 +4,10 @@
       <VideoPlayer :stream-manager="streamManager" :is-muted="true"/>
     </div>
     <div v-else class="broadcast-image">
-      <img :src="broadcastImage" alt="Broadcast Image"/>
+      <img :src="broadcastImage" alt="Broadcast Image" />
+      <div class="overlay">
+        <div class="overlay-text">방송 대기중입니다</div>
+      </div>
     </div>
   </div>
 </template>
@@ -46,10 +49,31 @@ export default {
   object-fit: cover;
 }
 
-.broadcast-image img{
-  width: 1000px;
-  height: 849px;
-  filter: blur(5px)
+.broadcast-image {
+  position: relative;
 }
 
+
+.broadcast-image img {
+  width: 1000px;
+  height: 849px;
+}
+
+.overlay {
+  position: absolute;
+  top: 0;
+  left: 0;
+  width: 100%;
+  height: 100%;
+  background-color: rgba(0, 0, 0, 0.5); /* 불투명한 박스 */
+  display: flex;
+  justify-content: center;
+  align-items: center;
+}
+
+.overlay-text {
+  color: white;
+  font-size: 2rem;
+  font-weight: bold;
+}
 </style>
