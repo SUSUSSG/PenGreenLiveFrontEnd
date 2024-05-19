ba6ca07ae450dc5c5167aa019e40b80ab729ce0d
JiniiW <ksjine2@gmail.com>
2024-05-19 16:19:40 +0900

    css : 버튼 스타일 변경

--- src/components/LiveBoard/liveboard-sidebar.vue
+++ src/components/LiveBoard/liveboard-sidebar.vue
@@ -3,34 +3,35 @@
     <div class="grid gap-8 mt-5" style="grid-template-rows: repeat(6, 1fr);">
       <div class="grid gap-8" style="grid-template-rows: repeat(6, 1fr);">
 
-        <div class="mx-auto h-10 w-10 flex flex-col items-center justify-center rounded-full bg-white text-2xl mb-4 black cursor-pointer"
-            @click="toggleIcon(statistics[0])">
+        <div
+          class="mx-auto h-10 w-10 flex flex-col items-center justify-center rounded-full bg-white text-2xl mb-4 black cursor-pointer"
+          @click="toggleIcon(statistics[0])">
           <Icon :icon="statistics[0].isActive ? statistics[0].activeIcon : statistics[0].icon">
             <!-- 카메라 조정 -->
           </Icon>
         </div>
 
-        <div class="mx-auto h-10 w-10 flex flex-col items-center justify-center rounded-full bg-white text-2xl mb-4 black cursor-pointer"
-            @click="toggleIcon(statistics[1])">
+        <div
+          class="mx-auto h-10 w-10 flex flex-col items-center justify-center rounded-full bg-white text-2xl mb-4 black cursor-pointer"
+          @click="toggleIcon(statistics[1])">
           <Icon :icon="statistics[1].isActive ? statistics[1].activeIcon : statistics[1].icon">
             <!-- 마이크 조정 -->
           </Icon>
         </div>
 
         <div>
-          <div class="mx-auto h-10 w-10 flex flex-col items-center justify-center rounded-full bg-white text-2xl mb-4 black cursor-pointer"
-              @click="openBroadcastDeviceControl()">
+          <div
+            class="mx-auto h-10 w-10 flex flex-col items-center justify-center rounded-full bg-white text-2xl mb-4 black cursor-pointer"
+            @click="openBroadcastDeviceControl()">
             <Icon :icon="statistics[2].icon"></Icon>
           </div>
-          <LivePrepareModal v-if="showLivePrepareModal"
-                            @broadcast-device-selected="handleDeviceSelection"
-                            title="방송 기기 설정" ref="broadcastDeviceControl"
-                            :showButtons="false" class="z-20"/>
+          <LivePrepareModal v-if="showLivePrepareModal" @broadcast-device-selected="handleDeviceSelection"
+            title="방송 기기 설정" ref="broadcastDeviceControl" :showButtons="false" class="z-20" />
         </div>
 
         <div
-            class="mx-auto h-10 w-10 flex flex-col items-center justify-center rounded-full bg-white text-2xl mb-4 black cursor-pointer"
-            @click="toggleIcon(statistics[3]); editBroadcastInfoModal()">
+          class="mx-auto h-10 w-10 flex flex-col items-center justify-center rounded-full bg-white text-2xl mb-4 black cursor-pointer"
+          @click="toggleIcon(statistics[3]); editBroadcastInfoModal()">
           <Icon :icon="statistics[3].icon"></Icon>
           <Modal title="방송 정보 편집" ref="editBroadcastInfo" :showButtons="false" :sizeClass="'max-w-2xl'">
             <div class="space-y-4">
@@ -39,9 +40,9 @@
                 <div class="flex flex-col">
                   <label for="boardcastTitle" class="block text-sm font-medium text-gray-700">방송
                     제목</label>
-                  <input id="newBoardcastTitle" type="text" v-model="newBroadcastTitle"
-                         placeholder="방송 제목 입력" @input="updateNewBroadcastTitle"
-                         class="flex-grow block w-full min-w-0 border-gray-300 focus:ring-blue-500 focus:border-blue-500 rounded-l-md mt-3"/>
+                  <input id="newBoardcastTitle" type="text" v-model="newBroadcastTitle" placeholder="방송 제목 입력"
+                    @input="updateNewBroadcastTitle"
+                    class="flex-grow block w-full min-w-0 border-gray-300 focus:ring-blue-500 focus:border-blue-500 rounded-l-md mt-3" />
                 </div>
                 <div class="flex items-center">
                   <Button btnClass="btn-primary btn-sm">
@@ -53,18 +54,17 @@
               <!-- 방송 썸네일 -->
               <div class="flex items-center justify-between">
                 <div class="flex flex-col">
-                  <label for="boardcastThumbnail"
-                         class="block text-sm font-medium text-gray-700 mt-10">썸네일</label>
+                  <label for="boardcastThumbnail" class="block text-sm font-medium text-gray-700 mt-10">썸네일</label>
                   <div class="flex items-center">
                     <div class="flex-shrink-0">
                       <img src="https://via.placeholder.com/90x160" alt="대표 이미지 미리보기" style="max-width: 400px;"
-                           class="mt-2"/>
+                        class="mt-2" />
                     </div>
                     <div class="ml-4">
                       <p>최대 용량 : 1mb</p>
                       <p>권장 사이즈 : 720 x 1280</p>
                       <input type="file" id="imageUpload" @change="handleImageUpload"
-                             accept="image/jpeg, image/png, image/gif" class="mb-2"/>
+                        accept="image/jpeg, image/png, image/gif" class="mb-2" />
                     </div>
                   </div>
                 </div>
@@ -79,8 +79,8 @@
         </div>
 
         <div
-            class="mx-auto h-10 w-10 flex flex-col items-center justify-center rounded-full bg-white text-2xl mb-4 black cursor-pointer"
-            @click="toggleIcon(statistics[4]); addNoticeModal()">
+          class="mx-auto h-10 w-10 flex flex-col items-center justify-center rounded-full bg-white text-2xl mb-4 black cursor-pointer"
+          @click="toggleIcon(statistics[4]); addNoticeModal()">
           <Icon :icon="statistics[4].icon"></Icon>
           <Modal title="공지사항 등록" ref="addNoticeModal" :showButtons="false">
             <!-- 공지사항 등록 -->
@@ -88,7 +88,7 @@
               <label for="addNotice" class="text-sm font-medium text-gray-700">공지사항 등록</label>
               <div class="flex items-center space-x-2">
                 <input id="addNotice" type="text" name="addNotice" v-model="notice" placeholder="공지사항 입력"
-                       class="flex-grow block w-full min-w-0 border-gray-300 focus:ring-blue-500 focus:border-blue-500 rounded-l-md"/>
+                  class="flex-grow block w-full min-w-0 border-gray-300 focus:ring-blue-500 focus:border-blue-500 rounded-l-md" />
                 <Button btnClass="btn-primary btn-sm" @click="submitNotice()">
                   등록
                 </Button>
@@ -99,36 +99,33 @@
               <label for="addNotice" class="text-sm font-medium text-gray-700">공지사항 목록</label>
               <div class="notice-list">
                 <li v-for="notice in notices" :key="notice.noticeSeq">{{ notice.noticeContent }}</li>
-                <button @click="removeNotice(index)"
-                        class="bg-red-500 hover:bg-red-600 text-white rounded p-1">
-                  <Icon icon="heroicons-outline:x"/>
-                </button>
+                <Icon icon="heroicons-outline:x" @click="removeNotice(index)" class="removeButton" />
               </div>
             </div>
           </Modal>
         </div>
 
         <div
-            class="mx-auto h-10 w-10 flex flex-col items-center justify-center rounded-full bg-white text-2xl mb-4 black cursor-pointer"
-            @click="toggleIcon(statistics[5]); addFaqModal()">
+          class="mx-auto h-10 w-10 flex flex-col items-center justify-center rounded-full bg-white text-2xl mb-4 black cursor-pointer"
+          @click="toggleIcon(statistics[5]); addFaqModal()">
           <Icon :icon="statistics[5].icon"></Icon>
-          <Modal title="자주 묻는 질문 등록" ref="addFaqRef" :showButtons="false" id="addFaq" :sizeClass="'max-w-5xl'">
+          <Modal title="자주 묻는 질문 등록" ref="addFaqRef" :showButtons="false" id="addFaq" :sizeClass="'max-w-6xl'">
             <div class="flex">
               <div class="w-1/2">
                 <div class="flex flex-col">
                   <div class="flex items-center mb-2">
                     <label for="addFaq" class="text-sm font-medium text-gray-700 mr-4 w-20">질문
                       등록</label>
-                    <textarea id="addQuestion" type="text" name="addFaq" placeholder="여기에 작성해주세요"
-                              rows="1" v-model="question"
-                              class="flex-grow block w-full min-w-0 border-gray-300 focus:ring-black-500 focus:border-black-500 rounded-l-md"></textarea>
+                    <textarea id="addQuestion" type="text" name="addFaq" placeholder="여기에 작성해주세요" rows="1"
+                      v-model="question"
+                      class="flex-grow block w-full min-w-0 border-gray-300 focus:ring-black-500 focus:border-black-500 rounded-l-md"></textarea>
                   </div>
                   <div class="flex items-center mb-2">
                     <label for="addFaq" class="text-sm font-medium text-gray-700 mr-4 w-20">답변
                       등록</label>
                     <textarea id="addAnswer" type="text" name="addFaq" placeholder="여기에 작성해주세요" rows="5"
-                              v-model="answer"
-                              class="flex-grow block w-full min-w-0 border-gray-300 focus:ring-blue-500 focus:border-black-500 rounded-l-md"></textarea>
+                      v-model="answer"
+                      class="flex-grow block w-full min-w-0 border-gray-300 focus:ring-blue-500 focus:border-black-500 rounded-l-md"></textarea>
                   </div>
                   <Button btnClass="btn-primary btn-sm mt-5" @click="submitFaq()">
                     등록
@@ -140,13 +137,12 @@
                   <label for="addFaq" class="text-sm font-medium text-gray-700">기존 목록</label>
                   <div class="faq-list" v-for="(faq, index) in faqs" :key="faq.faqSeq">
                     <div class="flex justify-between items-center">
-                      <dt :data-question="`Q: ${faq.question}`">{{ faq.questionTitle }}</dt>
-                      <button @click="removeFaq(index)"
-                              class="bg-red-500 hover:bg-red-600 text-white rounded p-1">
-                        <Icon icon="heroicons-outline:x"/>
-                      </button>
+                      <Icon icon="heroicons-outline:x" @click="removeFaq(index)" class="removeButton mr-3" />
+                      <div>
+                        <dt :data-question="`Q: ${faq.question}`">{{ faq.questionTitle }}</dt>
+                        <dd>{{ faq.questionAnswer }}</dd>
+                      </div>
                     </div>
-                    <dd>{{ faq.questionAnswer }}</dd>
                   </div>
                 </div>
               </div>
@@ -262,7 +258,7 @@ export default {
     },
     submitFaq() {
       if (this.question.trim() && this.answer.trim()) {
-        this.FaqList.push({question: this.question, answer: this.answer});
+        this.FaqList.push({ question: this.question, answer: this.answer });
         this.question = '';
         this.answer = '';
       }
@@ -305,9 +301,12 @@ export default {
 .faq-list {
   display: flex;
   flex-direction: column;
-  background-color: #f7fafc; /* Tailwind CSS의 bg-gray-100 */
-  padding: 0.5rem; /* Tailwind CSS의 p-2, 2 * 0.25rem = 0.5rem */
-  border-radius: 0.25rem; /* Tailwind CSS의 rounded */
+  background-color: #f7fafc;
+  /* Tailwind CSS의 bg-gray-100 */
+  padding: 0.5rem;
+  /* Tailwind CSS의 p-2, 2 * 0.25rem = 0.5rem */
+  border-radius: 0.25rem;
+  /* Tailwind CSS의 rounded */
 }
 
 li {
@@ -318,4 +317,10 @@ dt {
   font-weight: bold;
 }
 
+.removeButton {
+  background-color: rgb(251, 56, 56);
+  color: #fff;
+  border-radius: 0.25rem;
+  padding: 0.25rem;
+}
 </style>
