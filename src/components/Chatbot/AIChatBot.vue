6c9aeb2accce532058e2775ba7ad1bc3a8ff87f0
hinata7010 <1234minseok@naver.com>
2024-05-23 21:30:09 +0900

    refactor : 절대 경로 수정

--- src/components/Chatbot/AIChatBot.vue
+++ src/components/Chatbot/AIChatBot.vue
@@ -12,7 +12,7 @@
         <LottieAnimation
           @click="toggleChatbot"
           class="fixed-lottie"
-          animationPath="src/assets/images/all-img/penguinLottie.json"
+          animationPath="/src/assets/images/all-img/penguinLottie.json"
         />
         <span>챗봇 슈슈슉</span>
       </div>
