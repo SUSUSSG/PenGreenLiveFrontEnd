d55fcc044b03635f5dff4d8c57f7dfc3aa9e636f
장서윤 <seoy316@naver.com>
2024-05-24 16:16:32 +0900

    feat: 권한 삭제

--- src/plugins/axios.js
+++ src/plugins/axios.js
@@ -7,7 +7,6 @@ const axiosClient = axios.create({
 })
 
 axiosClient.interceptors.request.use(config => {
-    config.headers.Authorization = `Bearer ${store.state.user.token}`
     return config;
 })
 
