d7e4ddc750230d899850a4db7757b7b891d1089a
장서윤 <seoy316@naver.com>
2024-05-27 12:28:56 +0900

    feat: 프로필 이미지 설정
    진행중

--- src/views/mypage/edit-profile.vue
+++ src/views/mypage/edit-profile.vue
@@ -6,6 +6,26 @@
                     <div class="vgt-inner-wrap">
                         <div class="vgt-fixed-header"></div>
                         <div class="vgt-responsive">
+                            <div class="d-flex justify-content-center" style="margin-bottom: 3rem">
+                                <div class="text-center">
+                                <div class="user-img">
+                                    <img :src="form.userProfileImg ? form.userProfileImg : defaultImage" alt="Profile Picture"
+                                        class="profile profile-image-preview"
+                                        style="cursor: pointer;" @click="toggleModal"/>
+                                </div>
+                                <input type="file" ref="fileInput" accept="image/*" style="display: none;" @change="onFileChange">
+                                </div>
+                            </div>
+
+             
+                            <div class="modify-button-group">
+                                <div class="modify-btn-group-vertical">
+                                    <button class="btn modify-btn" @click="selectImage" type="button">사진 선택</button>
+                                    <button class="btn modify-btn" @click="changeDefaultImg" type="button">기본 이미지로 변경</button>
+                                </div>
+                            </div>
+        
+                            
                             <table id="vgt-table" class="vgt-table">
                                 <colgroup>
                                     <col id="col-0">
@@ -34,32 +54,32 @@
                                             <div class="mb-5">
                                                 <div class="flex items-center">                                                        
                                                     <div class="flex items-center flex-grow-7">
-                                                        <input type="text" value="010" class=" w-[20%] mr-2 input-control focus:outline-none h-[40px]" id="pn3" readonly="" disabled="">
-                                                        <input type="text" placeholder="휴대폰번호 (-없이 입력)" class="w-full input-control focus:outline-none h-[40px]" id="pn">
+                                                        <input type="text" value="010" class=" w-[20%] mr-2 input-control focus:outline-none h-[40px]" readonly="" disabled="">
+                                                        <input v-model="form.userTel" :disabled="responseAuth" type="text" placeholder="휴대폰번호 (-없이 입력)" class="w-full input-control focus:outline-none h-[40px]"
+                                                        >
                                                     </div>
                                                     <div class="w-[163px]">
-                                                        <Button text="인증번호 받기" class="w-full"/>
+                                                        <Button :disabled="responseAuth" @click="requestPhoneAuthCode" type="button" text="인증번호 받기" class="w-full"/>
                                                     </div>
                                                 </div>                                    
                                             </div>
-                                            <div class="grid-cols-1 grid mb-5 last:mb-0">
+                                            <div class="grid-cols-1 grid mb-5 last:mb-0" v-if="requestAuth">
                                                 <div class="flex items-center"> 
-                                                    <input type="text" name="pn" placeholder="인증번호 6자리 입력" class="flex-grow-7 input-control focus:outline-none h-[40px]" id="pn">
+                                                    <input v-model="inputAuthCode" type="text" placeholder="인증번호 6자리 입력" class="flex-grow-7 input-control focus:outline-none h-[40px]">
                                                     <div class="flex-grow-3">
-                                                        <Button text="인증번호 확인" class="w-full"/>
+                                                        <Button @click="verifyCode" type="button" text="인증번호 확인" class="w-full"/>
                                                     </div>
                                                 </div> 
                                             </div>
                                         </td>
                                     </tr>
-                                    
                                     <tr class="">
                                         <td class="vgt-left-align">
                                             <span>비밀번호</span>
                                         </td>
                                         <td class="vgt-right-align">
                                             <div class="flex">
-                                                <input type="password" name="pn" placeholder="비밀번호 (8~20자 영문+숫자)" class="flex-grow-7 classinput input-control w-full block focus:outline-none h-[40px]" id="pn">
+                                                <input v-model="form.userPw" type="password" placeholder="비밀번호 (8~20자 영문+숫자)" class="flex-grow-7 classinput input-control w-full block focus:outline-none h-[40px]">
                                                 <div class="flex-grow-3"></div>
                                             </div>
                                         </td>
@@ -70,18 +90,18 @@
                                         </td>
                                         <td class="vgt-right-align">
                                             <div class="flex">
-                                                <input type="password" name="pn" placeholder="비밀번호 확인 (8~20자 영문+숫자)" class="flex-grow-7 classinput input-control block focus:outline-none h-[40px]" id="pn">
+                                                <input v-model="confirmPassword" type="password" placeholder="비밀번호 확인 (8~20자 영문+숫자)" class="flex-grow-7 classinput input-control block focus:outline-none h-[40px]">
                                                 <div class="flex-grow-3"></div>
                                             </div>
                                         </td>
                                     </tr>
-                                    <tr class="">
+                                    <tr>
                                         <td class="vgt-left-align">
                                             <span>이메일</span>
                                         </td>
                                         <td class="vgt-right-align">
                                             <div class="flex">
-                                                <input v-model="user.userEmail" type="email" placeholder="이메일 입력" class="flex-grow-7 classinput input-control w-full block focus:outline-none h-[40px]" id="pn">
+                                                <input v-model="form.userEmail" type="email" placeholder="이메일 입력" class="flex-grow-7 classinput input-control w-full block focus:outline-none h-[40px]">
                                                 <div class="flex-grow-3"></div>
                                             </div>
                                         </td>
@@ -128,17 +148,18 @@
 
 <script setup>    
     import Button from "@/components/Button";
+    import Modal from "@/components/Modal"
     import Checkbox from "@/components/Checkbox";
     import Textarea from "@/components/Textarea";
     import axios from "@/axios";
     import { onMounted, ref } from 'vue';
     import Map from "@/components/Map/map.vue";
-import { add } from "@amcharts/amcharts5/.internal/core/util/Time";
+    import { add } from "@amcharts/amcharts5/.internal/core/util/Time";
+    // import imageCompression from 'browser-image-compression';
 
     onMounted(() =>{
         getUserInfo();
     });
-
     
     const form = ref({
         userBirthDt: null,
@@ -146,9 +167,8 @@ import { add } from "@amcharts/amcharts5/.internal/core/util/Time";
         userEmail: "",
         userAddress: "",
         userPw: "",
+        userProfileImg: null,
     });
-
-    
     
     const user = ref({});
 
@@ -166,6 +186,122 @@ import { add } from "@amcharts/amcharts5/.internal/core/util/Time";
         }
     }
 
+    // 휴대폰 번호 인증
+    let requestAuth = ref(false);
+    let responseAuth = ref(false);        
+    let inputAuthCode = ref(null);
+    let userTelVerify = ref(false);
+
+    function phoneNumberCheck(number){
+        let result = /^(01[016789]{1})-?[0-9]{3,4}-?[0-9]{4}$/;
+        return result.test(number);
+    }
+
+    function initPhoneNumberInput() {
+        responseAuth.value = false;
+        requestAuth.value = false;
+        form.value.userTel = "";
+    }
+
+    async function requestPhoneAuthCode() {
+        const phoneNumber = `010${form.value.userTel}`;
+
+        if (phoneNumber==="010") {
+            alert("휴대폰 번호를 입력하세요.");
+            return;
+        }
+        else if (!phoneNumberCheck(phoneNumber)) {
+            alert("휴대폰번호 입력 형식이 다릅니다.");
+            return;
+        } 
+
+        const params = {
+            phoneNumber: phoneNumber
+        }
+
+        try {
+            responseAuth.value = true;
+            const response = await axios.post(`/sms/request-authcode`, null, {params});
+
+            if (response.status===200) {
+                console.log(response.statusText);
+                requestAuth.value = true;
+            } else {
+                console.log(response.statusText);
+            }
+        } catch (error) {
+            alert('Error :', error.response.data);
+            responseAuth.value = false;
+            requestAuth.value = false;
+        }
+    }
+
+    async function verifyCode() {
+        const phoneNumber = `010${form.value.userTel}`;
+
+        if (inputAuthCode.value===null) {
+            alert("인증번호를 입력해주세요.");
+            return;
+        }
+
+        const params = {
+            phoneNumber: phoneNumber,
+            code: inputAuthCode.value
+        }
+
+        try {
+            const response = await axios.post('/sms/verify', null, {params});
+            if (response.status === 200) {
+                console.log(response.data);
+                alert("인증되었습니다.");
+                requestAuth.value = false;
+                userTelVerify.value = true;
+            } else {
+                alert(response.data);
+            }
+        } catch (error) {
+            if (error.response) {
+                if (error.response.status === 401) {    // 인증번호 틀림
+                    alert(error.response.data);
+                } else if(error.response.status === 410) {  // 인증시간 만료
+                    alert(error.response.data);
+                    initPhoneNumberInput();
+                }
+                else {
+                    alert(`서버 오류: ${error.response.tbody}`);
+                    responseAuth.value = false;
+                    requestAuth.value = false;
+                    form.value.userTel = "";
+                }
+            } else if (error.request) {
+                console.error('Error request:', error.request);
+                alert('네트워크 오류가 발생했습니다. 나중에 다시 시도해주세요.');
+            } else {
+                console.error('Error message:', error.message);
+                alert('알 수 없는 오류가 발생했습니다. 나중에 다시 시도해주세요.');
+            }
+        }
+    }
+
+     // 비밀번호 일치 체크
+    let confirmPassword = ref(null);
+
+    function checkPasswordsMatch() {
+        return form.value.userPw === confirmPassword.value;
+    }
+
+    // 비밀번호 형식 검사
+    function validateUserPw() {
+        const pattern = /^[a-zA-Z0-9]{8,20}$/;
+        return pattern.test(form.value.userPw);
+    }
+
+    // 이메일 타입 체크
+    const emailError = ref(true);
+    function validateEmail() {
+        const emailPattern = /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/;
+        return emailPattern.test(form.value.userEmail);
+    }
 
     // 우편번호 찾기
     const showMapModal =ref(false);
@@ -231,6 +367,59 @@ import { add } from "@amcharts/amcharts5/.internal/core/util/Time";
         }
     }
 
+    // 프로필 이미지 변경
+    const defaultImage = '/src/assets/images/users/default-img.png';
+    const fileInput = ref(null);
+    const profilePic = ref(null);
+
+
+    const show = ref(false);
+    const toggleModal = () => {
+        show.value = !show.value;
+    };
+
+function selectImage() {
+  fileInput.value.click();
+}
+
+function changeDefaultImg() {
+    form.value.userProfileImg = defaultImage;
+}
+
+function onFileChange(event) {
+  const file = event.target.files[0];
+  if (file) {
+    if (!file.type.match("image/*")) {
+      alert("이미지 파일만 업로드할 수 있습니다.");
+      return;
+    }
+
+    compressImage(file).then(compressedFile => {
+      if (compressedFile) {
+        const reader = new FileReader();
+        reader.onload = function (e) {
+          form.value.userProfileImg = e.target.result;
+        };
+        reader.readAsDataURL(compressedFile);
+      }
+    }).catch(error => {
+      console.error("이미지 압축 오류:", error);
+    });
+  }
+}
+
+async function compressImage(inputFile) {
+  try {
+    const options = {
+      maxSizeMB: 0.5,
+      maxWidthOrHeight: 1000,
+    };
+    return await imageCompression(inputFile, options);
+  } catch (error) {
+    console.error("이미지 압축 오류", error);
+    return null;
+  }
+}
 </script>
   
 <style scoped>
@@ -258,6 +447,41 @@ import { add } from "@amcharts/amcharts5/.internal/core/util/Time";
 .vgt-left-align {
     /* border-right: 1px solid #ddd; */
     background : #f5f5f5;
+}
+
+
+img.profile-image-preview {
+    border-radius: 50%;
+    height: 10rem;
+    width: 10rem;
+    object-fit: cover;
+    object-position: center;
+}
+
+#modify-modal {
+    height: 10rem;
+}
+
+#modify-modal h4 {
+    font-weight: 600;
+}
+
+.modify-btn-group {
+    width: 100%;
+    height: 100%;
+}
+
+.modify-btn-group-vertical {
+    width: 100%;
+    height: 100%;
+}
+
+.modify-btn {
+    width: 100%;
+    height: 90%;
+    background: white;
+    text-align: left;
+    font-size: 1rem;
 }
 
 </style>
